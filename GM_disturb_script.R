efa5fc396d9cc0dc9052520e39a19b4141dd8bbe
camalmborg <malmborg@bu.edu>
2021-06-11 10:09:05 -0400

    random effects JAGS code added

--- GM_disturb_script.R
+++ GM_disturb_script.R
@@ -14,13 +14,13 @@ for(i in geoID){
   gm.data[[i]]<-read.csv(file)
 }
 
-#load condition score means for each site:
-obs<-list()
+#load condition score means for each site into matrix:
+obs<-matrix(NA,nrow=length(geoID),ncol=130)
 for (i in geoID){
-  obs[[i]]<-gm.data[[i]]$score_mean
+  obs[i,]<-gm.data[[i]]$score_mean
 }
 
-#### model with disturbance probability added:
+#----model with disturbance probability added:------
 pests = "
 model{
   
@@ -42,38 +42,72 @@ model{
   x[1] ~ dnorm(x_ic,tau_ic)
   tau_obs ~ dgamma(t_obs,a_obs)
   tau_add ~ dgamma(a_add,t_add)
-  R ~ dnorm(rmean,rprec)
-  p ~ dunif(0,1)
+  R ~ dnorm(rmean,rprec)  #rho growth term
+  p ~ dunif(0,1)  #disturbance probability
+  mu0 ~ dnorm(-5,1) #mean of disturbed state
+  pa0 ~ dgamma(1,1) #precision of disturbed state
+}
+"
+
+#----model with site random effect------
+pests.RE = "
+model{
+
+###Loop over individual sites
+for (s in 1:ns){
+
+  #### Data Model
+  for(t in 1:n){
+    y[s,t] ~ dnorm(x[s,t],tau_obs)
+  }
+  
+  #### Process Model
+  for(t in 2:n){
+    muN[s,t]<-R*x[s,t-1]
+    x[s,t] ~ dnorm(mu[s,t],tau_add)
+    muD[s,t] ~ dnorm(mu0,pa0)
+    D[s,t] ~ dbern(p)
+    mu[s,t] <- D[s,t]*muD[s,t] + (1-D[s,t])*muN[s,t]
+  }
+  
+  x[s,1]~dnorm(x_ic,tau_ic)
+  
+}#end loop over sites
+  
+  #### Priors
+  tau_obs ~ dgamma(t_obs,a_obs)
+  tau_add ~ dgamma(a_add,t_add)
+  R ~ dnorm(rmean,rprec)  #rho growth term
+  p ~ dunif(0,1)  #disturbance probability
   mu0 ~ dnorm(-5,1) #mean of disturbed state
   pa0 ~ dgamma(1,1) #precision of disturbed state
 }
 "
 
 #data and parameters for each site:
-data<-list()
-j.pests<-list()
-j.pests.out<-list()
-for (i in geoID){
-data[[i]] = list(y=obs[[i]], n=NT,
+
+data.RE = list(y=obs, n=NT, ns=length(geoID),
             x_ic=0, tau_ic=0.1,
             a_obs=0.1,t_obs=0.1,
             a_add=0.1,t_add=0.1,
             rmean=0,rprec=0.00001)
-}
+
+j.pests.RE<-list()
+j.pests.out.RE<-list()
 
 #initial state of model parameters
 nchain <- 3
 for (i in geoID){
-  samp<- sample(!is.na(obs[[i]]),length(obs[[i]]),replace=TRUE)
+  samp<- sample(!is.na(obs[i,]),length(obs[i,]),replace=TRUE)
   for(j in 1:nchain){
     init<-list(tau_add=1/var(diff(samp)),tau_obs=1/var(samp))
     }
 
-  j.pests[[i]] <- jags.model (file = textConnection(pests),
-                      data = data[[i]],
+  j.pests.RE[[i]] <- jags.model (file = textConnection(pests.RE),
+                      data = data.RE,
                       inits = init,
                       n.chains = 3)
-  j.pests.out[[i]] <- coda.samples (model = j.pests[[i]],
+  j.pests.out.RE[[i]] <- coda.samples (model = j.pests.RE[[i]],
                             variable.names = c("x","tau_add","tau_obs", "R", "p", "D", "mu0", "pa0"),
                             n.iter = 5000)
 }
@@ -124,6 +158,12 @@ for (i in geoID){
   pa0s[,i]<-out[[i]][,"pa0"]
 }
 hist(pa0s)
+
+
+#looking at tau values @ example site 48:
+hist(1/sqrt(out[[48]][,'tau_add']))
+hist(1/sqrt(out[[48]][,'tau_obs']))
+plot(out[[48]][,'tau_add'],out[[48]][,'tau_obs'])
 
 
 
