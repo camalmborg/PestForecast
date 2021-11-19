e4bd3283eb384672deb5ae6188a18072a67db1d1
camalmborg <malmborg@bu.edu>
2021-11-19 17:27:15 -0500

    plotting all points not just sample for cond scores and dmag

--- GM_daymet_visualization_script.R
+++ GM_daymet_visualization_script.R
@@ -13,16 +13,16 @@ samp<-sample(nsites,500)
 xl=c(5,33) #maxtemp max
 yl=c(min(cond.june,na.rm=T),max(cond.june,na.rm=T))
 plot(maxvar[[1]][3,], cond.june[1,], pch=20, ylim=yl, xlim=xl)
-for (s in samp){
+for (s in nsites){
   points(maxvar[[s]][3,], cond.june[s,], pch=20)
 }
 
 #with disturbance mag as x:
-samp<-sample(nsites,500)
-xl=c(9,18) #maxtemp max
+#samp<-sample(nsites,500)
+xl=c(25,37) #maxtemp max
 yl=c(0,max(mags,na.rm=T))
-plot(maxvar[[1]][1,23], mags[1], pch=20, ylim=yl, xlim=xl)
-for (s in samp){
-  points(maxvar[[s]][1,23], mags[s], pch=20)
+plot(maxvar[[1]][6,23], mags[1], pch=20, ylim=yl, xlim=xl)
+for (s in nsites){
+  points(maxvar[[s]][6,23], mags[s], pch=20)
 }
 
