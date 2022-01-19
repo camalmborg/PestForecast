9958a0fb4ebbd709f3582afa377530fdecade37a
camalmborg <malmborg@bu.edu>
2022-01-19 14:42:12 -0500

    building code to extract daymet data from lists

--- GM_daymet_exploratory_analyses_script.R
+++ GM_daymet_exploratory_analyses_script.R
@@ -1,2 +1,34 @@
 ### Daymet Exploratory Analyses Automation ###
+#
+# MEANVAR TO VARIABLE #####
+#meanvar will load list with  mean of:
+  #precip<-meanvar
+  #maxtemp<-meanvar
+  #mintemp<-meanvar
+  #solr<-meanvar
+  #vpd<-meanvar
+  #rm(meanvar) #gets rid of redundant variable
 
+#
+### Extracting daymet data from list #####
+#to get data for 5 prior years 2011-2015:
+prior5 <- c(17:21)
+#to get data for disturbance window 2016-2017:
+distwind <- c(22:23)
+#to get whole time:
+alltime <- c(1:26)
+#number of sites and years and months:
+nsites = 1:5000
+nyears = 26
+ms = 12
+
+x.p <- matrix(data=NA, nrow=5000)
+for (p in 1:length(alltime)){
+  x <- matrix(data = NA, nrow=5000, ncol=5)
+  for (m in 1:length(ms)){
+    for (s in nsites){
+      x[s,m] <- meanvar[[s]][ms[m],alltime[p]]
+    }
+  }
+  x.p <- cbind(x.p,x)
+}
