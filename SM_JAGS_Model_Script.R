1528e87e7491d6ff25bde9e2996cfb2cee8ab5a8
camalmborg <malmborg@bu.edu>
2022-03-31 12:39:02 -0400

    setting up model to run samples

--- SM_JAGS_Model_Script.R
+++ SM_JAGS_Model_Script.R
@@ -2,11 +2,78 @@
 #Initially the model ran on 50 test sites, not the full 5000 (4997) sites
 #New script updates the initial dataset build and includes the updated model
 
+#THE LIBRARIES
+library(rjags)
+library(coda)
+library(ecoforecastR)
+library()
+
 #load full dataset (5000 sites):
 #model uses condition scores, not TCG raw data
 cond.scores.mo<-read.csv("2020_07_10_sample_score_mean_MONTHLY.csv")
 condscores<-cond.scores.mo[,2:131]
-condscores.samp<-condscores[1:250,]
 
-#number of sites
+#random selection of sites for testing:
+smpl<-sample(5000,250)
+condscores.samp<-condscores[smpl,]
+
+#number of sites, timesteps:
 nsites = nrow(condscores.samp)
+NT = ncol(condscores.samp)
+
+#THE MODEL:
+spongy_disturb <- "model{
+
+###Loop over individual sites
+for (s in 1:ns){
+
+  #### Data Model
+  for(t in 1:n){
+    y[s,t] ~ dnorm(x[s,t],tau_obs)
+  }
+  
+  #### Process Model
+  for(t in 2:n){
+    muN[s,t]<-R*x[s,t-1] ##step 3: dealing with modeling R
+    x[s,t] ~ dnorm(mu[s,t],tau_add)
+    muD[s,t] ~ dnorm(mu0[s,t],pa0) ##step 1: process model on mu0
+    D[s,t] ~ dbern(p) ##step 2: adding process model here
+    mu[s,t] <- D[s,t]*muD[s,t] + (1-D[s,t])*muN[s,t]
+    mu0[s,t] <- beta0
+  
+  x[s,1]~dnorm(x_ic,tau_ic)
+  
+}#end loop over sites
+  
+  #### Priors
+  tau_obs ~ dgamma(t_obs,a_obs)
+  tau_add ~ dgamma(a_add,t_add)
+  R ~ dnorm(rmean,rprec)  #rho term
+  p ~ dunif(0,1)  #disturbance probability
+  beta0 ~ dnorm(-5,1) #mean of disturbed state
+  pa0 ~ dgamma(1,1) #precision of disturbed state
+  
+}
+"
+
+#MODEL INPUTS
+#data and parameters for sites model:
+data = list(y=condscores.samp, n=NT, ns=length(nsites),
+              x_ic=0, tau_ic=0.1,
+              a_obs=0.1,t_obs=0.1,
+              a_add=0.1,t_add=0.1,
+              rmean=0,rprec=0.00001)
+
+#initial state of model parameters
+init<-list()
+nchain <- 3
+for(j in 1:nchain){
+  samp<- sample(!is.na(condscores.samp),length(condscores.samp),replace=TRUE)
+  init[[j]]<-list(tau_add=1/var(diff(samp)),tau_obs=1/var(samp))
+}
+
+j.pests <- jags.model (file = textConnection(spongy_disturb),
+                       data = data,
+                       inits = init,
+                       n.chains = 3)
+
