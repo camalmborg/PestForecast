0d368fe20f42559e19c0dabd7e926fcf0b1ed90d
camalmborg <malmborg@bu.edu>
2022-04-14 12:28:31 -0400

    linked cty and pop density data

--- SM_anthro_var_script.R
+++ SM_anthro_var_script.R
@@ -5,7 +5,9 @@
 popdens<-read.csv("Anthro_Var_Data/County_Pop_Data_MA_CT_RI.csv")
 #sample point counties:
 pointcty<-read.csv("Anthro_Var_Data/point_counties.csv")
+#tcg.recov data to run exploratory analyses:
+tcg.recov<-read.csv("SM_distmagrecov_data.csv")
 
-#match points to counties
-cond.scores.mo<-cbind(cond.scores.mo,pointcty$NAME,pointcty$STATE_NAME)
+library(dplyr)
+ctypop<-left_join(pointcty, popdens, by = c("STATE_NAME" = "STATE_NAME", "NAME" = "NAME"))
 
