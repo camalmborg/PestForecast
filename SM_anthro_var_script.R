47c5a0edba984fcfbeb7cc87c38469a386f81617
camalmborg <malmborg@bu.edu>
2022-04-14 11:10:41 -0400

    human pop data

--- SM_anthro_var_script.R
+++ SM_anthro_var_script.R
@@ -1,1 +1,11 @@
 #This script is for wrangling the anthropogenic variables
+
+#load datasets
+#Human population density (county scale):
+popdens<-read.csv("Anthro_Var_Data/County_Pop_Data_MA_CT_RI.csv")
+#sample point counties:
+pointcty<-read.csv("Anthro_Var_Data/point_counties.csv")
+
+#match points to counties
+cond.scores.mo<-cbind(cond.scores.mo,pointcty$NAME,pointcty$STATE_NAME)
+
