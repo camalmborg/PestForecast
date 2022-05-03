057827fb0fc72b0201a134b6502f081bccd77451
camalmborg <malmborg@bu.edu>
2022-05-03 14:04:52 -0400

    minor changes for plotting and grabbing sites, splitting param and x/d loops for easier summaries etc

--- SM_JAGS_Output_test_script.R
+++ SM_JAGS_Output_test_script.R
@@ -14,57 +14,39 @@ param1<-c("beta0","tau_add","tau_obs", "pa0")
 #param2<-c("beta[3]","beta[4]","tau_add","tau_obs", "pa0")
 #param3<-c("beta[1]","beta[2]","beta[3]","beta[4]","tau_add","tau_obs", "pa0")
 
-#CODA SPLIT VERSION-----
-# i=1
-# load(file = paste0(model1,as.character(i),".RData"))
-# #make empty matrix with sample mcmc object's # of columns (1 per tracked param)
-# #out<-matrix(NA,ncol=ncol(jpout[[1]]))
-# 
-# #outp<-matrix(NA,ncol=4)
-# outxs<-matrix(NA,ncol=(ncol(jpout[[1]])-4)/2)
-# #grab each mcmc object of that model and convert to matrix:
-# for (i in 20:19){
-#   #load jpout:
-#   load(file = paste0(model1,as.character(i),".RData"))
-#   
-#   #split output:
-#   codaSplit <- function(jpout,pattern){
-#     out = list()
-#     mfit = as.matrix(jpout,chains=TRUE)
-#     pat.cols = grep(pattern,colnames(mfit),fixed=TRUE)
-#     chain.col = which(colnames(mfit)=="CHAIN")
-#     out[[1]] = mat2mcmc.list(mfit[,c(chain.col,pat.cols)])
-#     out[[2]]   = mat2mcmc.list(mfit[,-pat.cols])
-#     return(out)
-#   }
-#   
-#   mat2mcmc.list <- function(w) {
-#     temp <- list()
-#     chain.col <- which(colnames(w) == "CHAIN")
-#     for (i in unique(w[, "CHAIN"])) {
-#       temp[[i]] <- coda:::as.mcmc(w[w[, "CHAIN"] == i, -chain.col])
-#     }
-#     return(as.mcmc.list(temp))
-#   } ##to use: out <- codaSplit(jags.out,"like")
-#   
-#   #jpmx<-as.matrix(jpout)
-#   #out<-rbind(out,jpmx)
-#   #rm(jpmx)
-#   #outr<-out[-1]
-#   jpxs<-codaSplit(jpout,"x")
-#   xs<-as.matrix(jpxs)
-#   outx<-rbind(outxs,xs)
-#   rm(jpxs)
-# }
-# outx1<-outx[-1,] #removes the NA row
-# #out2<-out[-1,]
-# #out3<-out[-1,]
-# rm(jpout)
-# rm(outp)
+#MCMC VIS VERSION
+#the following code has two loops, one for saving the parameters for the model
+#and the other for saving x's and D's for certain sites
 
+###LOOP FOR THE MODELPARAMS----
+outptemp<-matrix(NA,ncol=length(param1)) #1, 2, or 3
+#run loop for collecting parameters:
+for (i in 20:5){
+  #load jpout:
+  load(file = paste0(model1,as.character(i),".RData")) #model 1, 2, or 3
+  
+  #split output:
+  jpps<-MCMCchains(jpout, params=param1) #choose right params
+  
+  #grab sites for x and D:
+  outptemp<-rbind(outptemp,jpps)
+  
+  #remove big stuff
+  rm(jpps)
+  rm(jpout)
+}
 
-#MCMC VIS VERSION-----
-outptemp<-matrix(NA,ncol=length(param1))
+outp<-outptemp[-1,] #removes the NA row
+rm(outptemp)
+#summaries of model parameters:
+summ1<-as.data.frame.matrix(summary(outp))
+#summ2<-as.data.frame.matrix(summary(outp))
+#summ3<-as.data.frame.matrix(summary(outp))
+
+###x and D loop----
+#choose site:
+siten=22
+#empty matrices:
 outxtemp<-matrix(NA,ncol=NT)
 outdtemp<-matrix(NA,ncol=NT-1)
 
@@ -73,38 +55,34 @@ for (i in 20:5){
   load(file = paste0(model1,as.character(i),".RData"))
   
   #split output:
-  jpps<-MCMCchains(jpout, params=param1)
+  #jpps<-MCMCchains(jpout, params=param1)
   jpds<-MCMCchains(jpout, params="D")
   jpxs<-MCMCchains(jpout, params = "x")
   
   #grab sites for x and D:
-  xparam<-which(colnames(jpxs) %in% paste0("x[22,",1:130,"]"))
-  dparam<-which(colnames(jpds) %in% paste0("D[22,",1:130,"]"))
+  xparam<-which(colnames(jpxs) %in% paste0("x[",siten,",",1:130,"]"))
+  dparam<-which(colnames(jpds) %in% paste0("D[,",1:130,"]"))
   xsite<-jpxs[,xparam]
   dsite<-jpds[,dparam]
-  outptemp<-rbind(outptemp,jpps)
+  #outptemp<-rbind(outptemp,jpps)
   outxtemp<-rbind(outxtemp,xsite)
   outdtemp<-rbind(outdtemp,dsite)
   
   #remove big stuff
-  rm(jpps)
+  #rm(jpps)
   rm(jpxs)
   rm(jpds)
   rm(xsite,dsite)
   rm(jpout)
 }
 
-outp<-outptemp[-1,] #removes the NA row
+#outp<-outptemp[-1,] #removes the NA row
 outx<-outxtemp[-1,]
 outd<-outdtemp[-1,]
-rm(outptemp)
+#rm(outptemp)
 rm(outxtemp)
 rm(outdtemp)
 
-#summaries of model parameters:
-summ1<-as.data.frame.matrix(summary(outp))
-#summ2<-as.data.frame.matrix(summary(outp))
-#summ3<-as.data.frame.matrix(summary(outp))
 
 ##VISUALIZATIONS-----
 #if you need the data again:
@@ -129,8 +107,9 @@ NT=length(time)
 
 #save single model plot details:
 tiff(paste0("509", mod, "_examp_site", siten,".tiff"), 
-     units="in", width=10, height=5, res=300)
-plot(ci.x.1[2,],type='l',ylim=range(condscores.samp,na.rm=TRUE),
+     units="in", width=12, height=5, res=300)
+plot(ci.x.1[2,],type='l',
+     ylim=c(min(ci.x.1,na.rm=TRUE),max(condscores.samp,na.rm=TRUE)),
      main=paste0(modelname),
      ylab="Forest Condition Score",
      col="red",
@@ -138,6 +117,18 @@ plot(ci.x.1[2,],type='l',ylim=range(condscores.samp,na.rm=TRUE),
      cex=1)
 ecoforecastR::ciEnvelope(time,ci.x.1[1,],ci.x.1[3,],col=ecoforecastR::col.alpha("indianred1",0.60))
 points(time,condscores.samp[i,],pch=16,cex=0.5,col="navyblue")
+dev.off()
+
+#save single model plot details: for D's
+tiff(paste0("509", mod, "_examp_site", siten,"distprob.tiff"), 
+     units="in", width=10, height=5, res=300)
+plot(time[2:NT],ci.d.1[2,],type='l',ylim=range(ci.d.1,na.rm=TRUE),
+     main=paste0(modelname," :Disturbance Probability"),
+     ylab="Disturbance Probability",
+     col="red",
+     xlab="Month",
+     cex=1)
+ecoforecastR::ciEnvelope(time[2:NT],ci.d.1[1,],ci.d.1[3,],col=ecoforecastR::col.alpha("indianred1",0.60))
 dev.off()
 
 #save all models plot details:
