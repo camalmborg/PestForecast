7657a8c4524c82fd22e96d1aa90597f553d5f8d7
camalmborg <malmborg@bu.edu>
2022-05-04 14:13:32 -0400

    finished running 509 plots

--- SM_JAGS_Output_test_script.R
+++ SM_JAGS_Output_test_script.R
@@ -6,27 +6,27 @@ library(MCMCvis)
 
 #select model:
 model1<-"Model_1_mu0_jpout_run2_"
-#model2<-"Model_2_precip5k_jpout_"
-#model3<-"Model_3_fullenv5k_jpout_"
+model2<-"Model_2_precip5k_jpout_run2_"
+model3<-"Model_3_fullenv5k_jpout_run2_"
 
 #select parameters:
 param1<-c("beta0","tau_add","tau_obs", "pa0")
-#param2<-c("beta[3]","beta[4]","tau_add","tau_obs", "pa0")
-#param3<-c("beta[1]","beta[2]","beta[3]","beta[4]","tau_add","tau_obs", "pa0")
+param2<-c("beta0", "beta", "tau_add","tau_obs", "pa0")
+param3<-c("beta0", "beta", "tau_add","tau_obs", "pa0")
 
 #MCMC VIS VERSION
 #the following code has two loops, one for saving the parameters for the model
 #and the other for saving x's and D's for certain sites
 
 ###LOOP FOR THE MODELPARAMS----
-outptemp<-matrix(NA,ncol=length(param1)) #1, 2, or 3
+outptemp<-matrix(NA,ncol=length(param1)) #1, 2 (+1), or 3(+3)
 #run loop for collecting parameters:
 for (i in 20:5){
   #load jpout:
   load(file = paste0(model1,as.character(i),".RData")) #model 1, 2, or 3
   
   #split output:
-  jpps<-MCMCchains(jpout, params=param1) #choose right params
+  jpps<-MCMCchains(jpout, params=param3) #choose right params
   
   #grab sites for x and D:
   outptemp<-rbind(outptemp,jpps)
@@ -45,14 +45,17 @@ summ1<-as.data.frame.matrix(summary(outp))
 
 ###x and D loop----
 #choose site:
-siten=22
+siten=68
+#time:
+time=1:130
+NT=length(time)
 #empty matrices:
 outxtemp<-matrix(NA,ncol=NT)
 outdtemp<-matrix(NA,ncol=NT-1)
 
-for (i in 20:5){
+for (i in 30:10){
   #load jpout:
-  load(file = paste0(model1,as.character(i),".RData"))
+  load(file = paste0(model3,as.character(i),".RData"))
   
   #split output:
   #jpps<-MCMCchains(jpout, params=param1)
@@ -61,7 +64,7 @@ for (i in 20:5){
   
   #grab sites for x and D:
   xparam<-which(colnames(jpxs) %in% paste0("x[",siten,",",1:130,"]"))
-  dparam<-which(colnames(jpds) %in% paste0("D[,",1:130,"]"))
+  dparam<-which(colnames(jpds) %in% paste0("D[",siten,",",1:130,"]"))
   xsite<-jpxs[,xparam]
   dsite<-jpds[,dparam]
   #outptemp<-rbind(outptemp,jpps)
@@ -89,17 +92,13 @@ rm(outdtemp)
 #condscores.samp<-data$y
 
 #confidence intervals:
-ci.x.1 <- apply(outx,2,quantile,c(0.025,0.5,0.975))
-ci.d.1 <- apply(outd,2,quantile,c(0.025,0.5,0.975))
-# ci.x.2 <- apply(outx,2,quantile,c(0.025,0.5,0.975))
-# ci.d.2 <- apply(outd,2,quantile,c(0.025,0.5,0.975))
-# ci.x.3 <- apply(outx,2,quantile,c(0.025,0.5,0.975))
-# ci.d.3 <- apply(outd,2,quantile,c(0.025,0.5,0.975))
+ci.x <- apply(outx,2,quantile,c(0.025,0.5,0.975))
+ci.d <- apply(outd,2,quantile,c(0.025,0.5,0.975))
 
 #details for quick file naming:
-siten<-22
-mod<-"Mod1"
-modelname<-"Model 1"
+#siten<-22
+mod<-"Mod3"
+modelname<-"Model 3"
 
 #time for plots:
 time=1:130
@@ -108,43 +107,68 @@ NT=length(time)
 #save single model plot details:
 tiff(paste0("509", mod, "_examp_site", siten,".tiff"), 
      units="in", width=12, height=5, res=300)
-plot(ci.x.1[2,],type='l',
-     ylim=c(min(ci.x.1,na.rm=TRUE),max(condscores.samp,na.rm=TRUE)),
+plot(ci.x[2,],type='l',
+     ylim=range(condscores.samp,na.rm=T),#c(min(ci.x,na.rm=TRUE),max(condscores.samp,na.rm=TRUE)),
      main=paste0(modelname),
      ylab="Forest Condition Score",
-     col="red",
+     col="dark green",
      xlab="Month",
      cex=1)
-ecoforecastR::ciEnvelope(time,ci.x.1[1,],ci.x.1[3,],col=ecoforecastR::col.alpha("indianred1",0.60))
+ecoforecastR::ciEnvelope(time,ci.x[1,],ci.x[3,],col=ecoforecastR::col.alpha("yellowgreen",0.60))
 points(time,condscores.samp[i,],pch=16,cex=0.5,col="navyblue")
 dev.off()
 
 #save single model plot details: for D's
 tiff(paste0("509", mod, "_examp_site", siten,"distprob.tiff"), 
      units="in", width=10, height=5, res=300)
-plot(time[2:NT],ci.d.1[2,],type='l',ylim=range(ci.d.1,na.rm=TRUE),
+plot(time[2:NT],ci.d[2,],type='l',ylim=range(ci.d,na.rm=TRUE),
      main=paste0(modelname," :Disturbance Probability"),
+     ylab="Disturbance Probability",
+     col="dark green",
+     xlab="Month",
+     cex=1)
+ecoforecastR::ciEnvelope(time[2:NT],ci.d[1,],ci.d[3,],col=ecoforecastR::col.alpha("yellowgreen",0.60))
+dev.off()
+
+
+#save all models plot details:
+ci.x.1 <- apply(outx,2,quantile,c(0.025,0.5,0.975))
+ci.d.1 <- apply(outd,2,quantile,c(0.025,0.5,0.975))
+ci.x.2 <- apply(outx,2,quantile,c(0.025,0.5,0.975))
+ci.d.2 <- apply(outd,2,quantile,c(0.025,0.5,0.975))
+ci.x.3 <- apply(outx,2,quantile,c(0.025,0.5,0.975))
+ci.d.3 <- apply(outd,2,quantile,c(0.025,0.5,0.975))
+
+tiff(paste0("509_allmodels_2_", siten, ".tiff"),
+     units="in", width=12, height=5, res=300)
+#plot:
+plot(time,ci.x.1[2,],type='l',ylim=range(condscores.samp,na.rm=T),
+     main="All Models",
+     ylab="Forest Condition Score",
+     col="red",
+     xlab="Month",
+     cex=1)
+lines(time,ci.x.2[2,], col="blue")
+lines(time,ci.x.3[2,], col="dark green")
+ecoforecastR::ciEnvelope(time,ci.x.1[1,],ci.x.1[3,],col=ecoforecastR::col.alpha("indianred1",0.30))
+ecoforecastR::ciEnvelope(time,ci.x.2[1,],ci.x.2[3,],col=ecoforecastR::col.alpha("lightblue1",0.30))
+ecoforecastR::ciEnvelope(time,ci.x.3[1,],ci.x.3[3,],col=ecoforecastR::col.alpha("greenyellow",0.30))
+points(time,condscores.samp[siten,],pch=16,cex=0.5,col="navyblue")
+dev.off()
+
+#plot for disturbance prob all models:
+tiff(paste0("509_allmodels_2_", siten, "_distprob", ".tiff"),
+     units="in", width=10, height=5, res=300)
+#plot:
+plot(time[2:NT],ci.d.1[2,],type='l',ylim=range(ci.d.1,na.rm=TRUE),
      ylab="Disturbance Probability",
      col="red",
      xlab="Month",
      cex=1)
-ecoforecastR::ciEnvelope(time[2:NT],ci.d.1[1,],ci.d.1[3,],col=ecoforecastR::col.alpha("indianred1",0.60))
+lines(time[2:NT],ci.d.2[2,], col="blue")
+lines(time[2:NT],ci.d.3[2,], col="dark green")
+ecoforecastR::ciEnvelope(time[2:NT],ci.d.1[1,],ci.d.1[3,],col=ecoforecastR::col.alpha("indianred1",0.30))
+ecoforecastR::ciEnvelope(time[2:NT],ci.d.2[1,],ci.d.2[3,],col=ecoforecastR::col.alpha("lightblue1",0.30))
+ecoforecastR::ciEnvelope(time[2:NT],ci.d.3[1,],ci.d.3[3,],col=ecoforecastR::col.alpha("greenyellow",0.30))
 dev.off()
-
-#save all models plot details:
-# tiff(paste0("509_allmodels_", siten, ".tiff"), 
-#      units="in", width=10, height=5, res=300)
-# plot:
-# plot(time,ci.x.1[2,],type='l',ylim=c(-12,5),
-#      ylab="Forest Condition Score", 
-#      col="red",
-#      xlab="Month",
-#      cex=1)
-# lines(time,ci.x.2[2,], col="blue")
-# lines(time,ci.x.3[2,], col="dark green")
-# ecoforecastR::ciEnvelope(time,ci.x.1[1,],ci.x.1[3,],col=ecoforecastR::col.alpha("indianred1",0.30))
-# ecoforecastR::ciEnvelope(time,ci.x.2[1,sitei],ci.x.2[3,sitei],col=ecoforecastR::col.alpha("lightblue1",0.30))
-# ecoforecastR::ciEnvelope(time,ci.x.3[1,sitei],ci.x.3[3,sitei],col=ecoforecastR::col.alpha("greenyellow",0.30))
-# points(time,condscores.samp[siten,],pch=16,cex=0.5,col="navyblue")
-# dev.off()
 
