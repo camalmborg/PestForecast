3d4dbfabcac0ddcbcb658d4fff026d6cf7ecce3f
camalmborg <malmborg@bu.edu>
2022-06-07 15:18:54 -0400

    dist prob code loop works, mext step: threshold

--- SM_dist_prob_exploratory_analyses_script.R
+++ SM_dist_prob_exploratory_analyses_script.R
@@ -3,12 +3,27 @@
 #load condition scores (finding where prob of dist = 0,1)
 cond.scores.mo<-read.csv("2020_07_10_sample_score_mean_MONTHLY.csv")
 condscores<-cond.scores.mo[,2:131]
+junes<-c(102,107,112)
+js<-condscores[,junes]
 
 #load tcg data for column numbers:
 distmagsdata<-read.csv("SM_distmagrecov_data.csv")
-
+dmdjs<-cbind(distmagsdata,js)
 
 #adding 0,1 for disturbance occurrence:
-for (i in nrow(condscores)){
-  
+#make empty matrix for 0,1 data:
+distprob<-matrix(NA,nrow=nrow(condscores),ncol=1)
+#disturbance threshold:
+d = -2
+#determine if disturbance (condscore < d threshold) occurs
+for (i in 1:nrow(condscores)){
+  if (dmdjs[i,]$colnum == 21 & dmdjs[i,]$X2015.06.01_score_mean <= d){
+    distprob[i,]<- 1
+    } else if (dmdjs[i,]$colnum == 22 & dmdjs[i,]$X2016.06.01_score_mean <= d){
+        distprob[i,]<- 1
+    } else if (dmdjs[i,]$colnum == 23 & dmdjs[i,]$X2017.06.01_score_mean <= d){
+        distprob[i,]<- 1
+    } else {
+        distprob[i,] <- 0
+            }
 }
