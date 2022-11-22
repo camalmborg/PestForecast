2ea723094437dd9720788dbfcc24996125fd8261
camalmborg <malmborg@bu.edu>
2022-11-22 12:14:48 -0500

    finally figured out how to make the daymet data sorting function to work! now automated

--- GM_daymet_data_script.R
+++ GM_daymet_data_script.R
@@ -3,6 +3,7 @@ library(daymetr)
 library(tidyr)
 library(stringr)
 
+###for if your data has lat lon columns:----
 ###load data
 #load condition scores:
 # file<-"2020_07_10_sample_score_mean_MONTHLY.csv"
@@ -14,7 +15,7 @@ library(stringr)
 # nsites<-1:nrow(cond.scores.mo)
 # sites<-cbind(nsites,coords)
 
-###if you have data with just geo col:
+###if you have data with just geo col:-----
 file<-"2022_08_31_DATAGRAB/2022_08_31_5k_score_mean - 2022_08_31_5k_score_mean.csv"
 cond.scores<-read.csv(file)
 geo<-as.data.frame(cond.scores[,".geo"])
@@ -36,17 +37,6 @@ colnames(coords)<-c("lon","lat")
 nsites<-1:nrow(cond.scores)
 sites<-as.data.frame(cbind(nsites,coords))
 
-
-###put daymet data into a list
-#for single site to get dm:
-# dm<-daymetr::download_daymet(site = sites$nsites[1],
-#                              lat = sites$lat[1],
-#                              lon = sites$lon[1],
-#                              start = 1995,
-#                              end = 2020,
-#                              internal = TRUE)
-
-
 ###loop version for putting daymet data into a list:
 #start and end
 startyr <-1995
@@ -67,29 +57,49 @@ for(i in nsites){
 doy <- dm[[1]]$data$yday
 
 #function for grabbing daymet data:
-spongy_met<-function(dmvar,var){
+spongy_met<-function(var,filenm){
   dmvar<-list()
   for (i in nsites){
     metyr<-dm[[i]]$data$year
     metyears<-unique(metyr)
     dmvar[[i]]<-matrix(NA,length(metyears),365)
     for(j in 1:nrow(dm[[i]]$data)){
-      dmvar[[i]][as.numeric(as.factor(metyr))[j], dm[[i]]$data$yday[j]]=dm[[i]]$data$var[j]
+      dmvar[[i]][as.numeric(as.factor(metyr))[j], 
+                 dm[[i]]$data$yday[j]]=dm[[i]]$data[[var]][j]
     }
   }
-  return(dmvar)
-  save(dmvar,file=paste0(deparse(substitute(dmvar)),".RData"))
+  #return(dmvar)
+  save(dmvar,file=paste0(filenm,".RData"))
 }
 
-spongy_met(maxtemp96,var)
-
-#^^^this code does not work correctly yet
+#choose your variable from the daymet list, add filename 
+#(as characters):
+spongy_met("tmax..deg.c.","maxtemp")
 
 #can do for variables:
-#maxtemp=
+#maxtemp=tmax..deg.c.
 #mintemp=tmin..deg.c.
 #vpd=vp..Pa.
 #precip=prcp..mm.day.
+
+#^^^this code works now
+
+
+###ARCHIVED CODE-----
+
+#these lines were for testing:
+#sites<-sites[1:5,]
+#nsites<-1:nrow(sites)
+
+###put daymet data into a list
+#for single site to get dm:
+# dm<-daymetr::download_daymet(site = sites$nsites[1],
+#                              lat = sites$lat[1],
+#                              lon = sites$lon[1],
+#                              start = 1995,
+#                              end = 2020,
+#                              internal = TRUE)
+
 
 ###individual variable loops:
 # maxtemp<-list()
@@ -107,75 +117,3 @@ spongy_met(maxtemp96,var)
 # #saveRDS(maxtemp, file='maxtemp.rds')
 # save(maxtemp, file='maxtemp.RData')
 # #load('maxtemp.RData')
-# 
-# 
-# #grabbing min temp from each site's daymet data in dm list
-# mintemp<-list()
-# for (i in nsites){
-#    metyr= dm[[i]]$data$year
-#    metyears=unique(metyr)
-#    mintemp[[i]]<-matrix(NA,length(metyears),365)
-#    for(j in 1:nrow(dm[[i]]$data)){
-#       mintemp[[i]][as.numeric(as.factor(metyr))[j],dm[[i]]$data$yday[j]]=dm[[i]]$data$tmin..deg.c.[j]
-#    }
-#    print("done!")
-# }
-# #this code works^^^
-# print("it worked!")
-# #saveRDS(mintemp, file='mintemp.rds')
-# save(mintemp, file='mintemp.RData')
-# #load('mintemp.RData')
-# 
-# 
-# #grabbing vpd from each site's daymet data in dm list
-# vpd<-list()
-# for (i in nsites){
-#    metyr= dm[[i]]$data$year
-#    metyears=unique(metyr)
-#    vpd[[i]]<-matrix(NA,length(metyears),365)
-#    for(j in 1:nrow(dm[[i]]$data)){
-#       vpd[[i]][as.numeric(as.factor(metyr))[j],dm[[i]]$data$yday[j]]=dm[[i]]$data$vp..Pa.[j]
-#    }
-#    print("done!")
-# }
-# #this code works^^^
-# print("it worked!")
-# #saveRDS(vpd, file='vpd.rds')
-# save(vpd, file='vpd.RData')
-# #load('mintemp.RData')
-# 
-# 
-# precip<-list()
-# for (i in nsites){
-#   metyr=dm[[i]]$data$year
-#   metyears=unique(metyr)
-#   precip[[i]]<-matrix(NA,length(metyears),365)
-#   for(j in 1:nrow(dm[[i]]$data)){
-#     precip[[i]][as.numeric(as.factor(metyr))[j],dm[[i]]$data$yday[j]]=dm[[i]]$data$prcp..mm.day.[j]
-#   }
-#   print("done!")
-# }
-# #this code works^^^
-# print("it worked!")
-# save(precip, file='precip.RData')
-# 
-# 
-# solr<-list()
-# for (i in nsites){
-#   metyr=dm[[i]]$data$year
-#   metyears=unique(metyr)
-#   solr[[i]]<-matrix(NA,length(metyears),365)
-#   for(j in 1:nrow(dm[[i]]$data)){
-#     solr[[i]][as.numeric(as.factor(metyr))[j],dm[[i]]$data$yday[j]]=dm[[i]]$data$srad..W.m.2.[j]
-#   }
-#   print("done!")
-# }
-# #this code works^^^
-# print("it worked!")
-# save(solr, file='solr.RData')
-
-#automating to run through x number of variables?
-#change to May-Sep grab instead of full year? 
-#   -no, we may want winter temps
-#   -separate seasons as needed later
-
