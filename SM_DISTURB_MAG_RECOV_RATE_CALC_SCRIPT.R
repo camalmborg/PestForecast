3c175fd8234dfe5f1574a0ce9184737f91894500
camalmborg <malmborg@bu.edu>
2022-12-07 15:12:39 -0500

    updating with monthly values file

--- SM_DISTURB_MAG_RECOV_RATE_CALC_SCRIPT.R
+++ SM_DISTURB_MAG_RECOV_RATE_CALC_SCRIPT.R
@@ -2,10 +2,14 @@
 
 #### Load condition score .csv from GEE extract:
 #file<-"2022_08_31_DATAGRAB/2022_08_31_5k_score_mean - 2022_08_31_5k_score_mean.csv"
-file<-"2022_08_31_DATAGRAB/2022_08_31_5k_tcg_mean - 2022_08_31_5k_tcg_mean.csv"
+#file<-"2022_08_31_DATAGRAB/2022_08_31_5k_tcg_mean - 2022_08_31_5k_tcg_mean.csv"
+file<-"2022_08_31_DATAGRAB/2022_12_7_sample_tcg_mean_5k.csv"
 
 #condition score object:
 #cond.scores<-read.csv(file)
 tcg.values<-read.csv(file)
-tcgs<-tcg.values[,c(grep("^X",colnames(tcg.values)))]
+tcgs<-tcg.values[,c(grep("^X",colnames(tcg.values)))] #grab with "X1996 eg) for all tcg value columns
+
+#grab june values:
+
 
