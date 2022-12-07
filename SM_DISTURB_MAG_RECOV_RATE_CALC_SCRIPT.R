0dd0a55a562531ab412e0f2445f09f6eaf404289
camalmborg <malmborg@bu.edu>
2022-12-07 15:50:50 -0500

    building function for calculating disturbance magnitude, disturbance probability, and recovery rates

--- SM_DISTURB_MAG_RECOV_RATE_CALC_SCRIPT.R
+++ SM_DISTURB_MAG_RECOV_RATE_CALC_SCRIPT.R
@@ -10,6 +10,18 @@ file<-"2022_08_31_DATAGRAB/2022_12_7_sample_tcg_mean_5k.csv"
 tcg.values<-read.csv(file)
 tcgs<-tcg.values[,c(grep("^X",colnames(tcg.values)))] #grab with "X1996 eg) for all tcg value columns
 
-#grab june values:
+#### Function for computing disturbance magnitudes, probabilities, and recovery rates:
+spongy_mpr<-function(tcg,distyr){
+  #first get just june values:
+  junes<-seq(2,130,by=5)
+  tcgjune<-as.matrix(tcg[,junes])
+  
+  #identify steady state (mean tcg previous 3 years):
+  prevyrs<-tcgjune[,grep(as.character(distyr-3),colnames(tcgjune)):
+                     grep(as.character(distyr-1),colnames(tcgjune))]
+  steadys<-as.matrix(apply(prevyrs[,1:3],1,mean,na.rm=T))
+  
+  #
+}
 
 
