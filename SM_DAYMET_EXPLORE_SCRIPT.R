61206aac0b74c491af173448e74ace10990a7a9c
camalmborg <malmborg@bu.edu>
2022-12-13 13:19:53 -0500

    fixed indexing in r2 table, now fills in

--- SM_DAYMET_EXPLORE_SCRIPT.R
+++ SM_DAYMET_EXPLORE_SCRIPT.R
@@ -11,14 +11,14 @@ dm_explore<-function(dmvars,dmrdat,dmr,coln){
   #loop over all members of dmvars list:
   for (i in 1:length(dmvars)){
     #first extract the list you want:
-    dmvariable <- as.data.frame(dmvars[[1]][as.numeric(dmrdat$sitenum),])
+    dmvariable <- as.data.frame(dmvars[[i]][as.numeric(dmrdat$sitenum),])
     #grab column number:
-    cn <- as.matrix(as.numeric(dmr$colnum))
+    cn <- as.matrix(as.numeric(dmrdat$colnum))
     #grab exploratory response  of choice:
-    y <- as.matrix(as.numeric(dmr$mags))
+    y <- as.matrix(as.numeric(dmrdat[,dmr]))
     x <- as.data.frame(cbind(y,cn,dmvariable))
     vardat <- x[x$cn==coln,]
-    
+  
     #loop for filling in R2 table:  
       for (j in 1:ncol(dmvariable)){
         var.gam <- gam(vardat[,1]~s(vardat[,j+2]),data=vardat)
@@ -26,6 +26,9 @@ dm_explore<-function(dmvars,dmrdat,dmr,coln){
         #extract r2:
         summ <- summary(var.gam)
         r2s[j,i] <-summ$r.sq
-    }
-  }
+      }
+   }
+  return(r2s)
 }
+
+testing <- dm_explore(dmvars,testfx,"mags",22)
