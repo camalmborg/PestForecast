0f4cac8b96aea0d6ac135edea164e219ea5be156
camalmborg <malmborg@bu.edu>
2023-01-12 12:40:11 -0500

    replacing

--- SM_DISTURB_PROB_EXPLORE.R
+++ SM_DISTURB_PROB_EXPLORE.R
@@ -4,3 +4,9 @@
 cfile<-"2022_08_31_DATAGRAB/2022_12_7_sample_score_mean_5k.csv"
 condscores<-read.csv(cfile)
 
+spongy_dp<-function(cs,){
+  junes<-cs[,grep("[:.:]06",colnames(cs))]
+  
+}
+
+junetest<-condscores[,grep("[:.:]06",colnames(condscores))]
