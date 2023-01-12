51386ab2c1f6f1eb13b7c79780efe887c4e4b51f
camalmborg <malmborg@bu.edu>
2023-01-12 12:11:23 -0500

    first commit: clean disturbance probability code

---
+++ SM_DISTURB_PROB_COMPUTE_EXPLORE.R
@@ -0,0 +1,6 @@
+#code for disturbance probability exploratory analyses
+
+### Load condition score data:
+cfile<-"2022_08_31_DATAGRAB/2022_12_7_sample_score_mean_5k.csv"
+condscores<-read.csv(cfile)
+
