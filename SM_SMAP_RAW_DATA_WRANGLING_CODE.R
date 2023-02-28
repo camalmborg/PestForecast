f7afa52849595211f1bb84c8e657669cc9013dd4
camalmborg <malmborg@bu.edu>
2023-02-28 15:22:47 -0500

    first commit for smap raw data wranglin'

---
+++ SM_SMAP_RAW_DATA_WRANGLING_CODE.R
@@ -0,0 +1,5 @@
+#this code is for the SMAP data- raw monthly values for 2015-2017 from GEE for 
+#univariate analyses. The R SMAP download code is not set up for 5000 sites of
+#data and so I'm going rogue to get my initial analyses done.
+
+#load libraries
