f59c57f65a87678ea4db0ba584f5388f92a9e848
camalmborg <malmborg@bu.edu>
2023-03-10 11:37:48 -0500

    not sure how to resolve the missing problem but happy to just use the environment with "missing" object in it for now

--- SM_SMAP_RAW_DATA_WRANGLING_CODE.R
+++ SM_SMAP_RAW_DATA_WRANGLING_CODE.R
@@ -4,13 +4,19 @@
 
 #load libraries
 library(mgcv)
+library(tidyr)
+library(stringr)
 
 #load data
-file <- "SMAP_Data/SMAP_04_08_2015_2017_try3.txt"
-SMAP.GEE <- read.table(file, header=T, sep=",")
+#file<-"2022_08_31_DATAGRAB/2022_12_7_sample_tcg_mean_5k.csv"
+#cfile <- cfile<-"2022_08_31_DATAGRAB/2022_12_7_sample_score_mean_5k.csv"
+sfile <- "SMAP_Data/SMAP_04_08_2015_2017_try3.txt"
+SMAP.GEE <- read.table(sfile, header=T, sep=",")
 #load lat long data from sampling points:
 sitesfile <- "2022_03_22_5000sites_lat_long_points_for_GEE_asset.csv"
 sites <- read.csv(sitesfile)
+#cond.scores<-read.csv(cfile)
+
 
 #get rid of ] from smap data, make numeric:
 SMAPvalues <- SMAP.GEE[,"smp."]
@@ -23,9 +29,26 @@ SMAPdata <- as.data.frame(cbind(SMAP.GEE[,"longitude"],
                                 SMAP.GEE[,"latitude"],
                                 SMAPvalues))
 
+#make coords object:
+# geo<-as.data.frame(cond.scores[,".geo"])
+# #make lat and lon columns from .geo data:
+# coords<-matrix(nrow=nrow(geo),ncol=3)
+# for (i in 1:nrow(geo)){
+#   #longitudes:
+#   lon<-str_extract(geo[i,], "\\d+\\.*\\d*")
+#   coords[i,1]<-as.numeric(lon)*-1
+#   
+#   #latitudes:
+#   extlon<-sub(lon,"",geo[i,])
+#   coords[i,2]<-as.numeric(str_extract(extlon, "\\d+\\.*\\d*"))
+#   coords[i,3]<-i
+# }
+# colnames(coords)<-c("lon","lat","site_id")
+coords <- cbind(c(1:5000),sites[,2],sites[,1])
+colnames(coords)<-c("site_id","lon","lat")
 
 #how many sites:
-nsites=5000
+nsites=nrow(sites)
 #loop for organizing SMAP data into monthly columns 2015-2017:
 SMAPorg <- matrix(data=NA, nrow=nsites, ncol=15) #15= 3 years X 5 months
 for (i in 1:15){
@@ -36,10 +59,10 @@ for (i in 1:15){
 
 ####matching SMAP lat/lon to mags lat/lon
 #make data frame with coords and mags:
-magsdat <- as.data.frame(cbind(coords[-missing,],mags))
+#magsdat <- as.data.frame(cbind(coords[-missing,],mags))
 #magscoords <- magsdat[order(magsdat[,1]),]
 
-sortcoords <- as.data.frame(coords[order(coords[,1]),]) #with full 5000
+sortcoords <- as.data.frame(coords[order(coords[,2]),]) #with full 5000
 
 #remove duplicate values in SMAP lat/lon:
 SMAPlonlat <- as.data.frame(unique(SMAPdata[,1:2]))
@@ -50,7 +73,10 @@ SMAPcoords <- SMAPall[order(SMAPall[,1]),]
 SMAPsmap <- cbind(sortcoords$site_id,SMAPcoords)
 sortSMAP <- SMAPsmap[order(SMAPsmap[,1]),]
 #removing correct missing values:
-sortSMAP <- sortSMAP[-missing,]
+#sortSMAP <- sortSMAP[-missing,]
+
 
 #make new data frame:
 SMAPmags <- cbind(magsdat, sortSMAP[,4:ncol(sortSMAP)])
+
+
