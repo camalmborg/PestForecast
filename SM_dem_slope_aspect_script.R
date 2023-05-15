0fb84265d350e04faf8424060fd9bddcc21a13df
camalmborg <malmborg@bu.edu>
2023-05-15 13:26:45 -0400

    trying to reproject and crop raster

--- SM_dem_slope_aspect_script.R
+++ SM_dem_slope_aspect_script.R
@@ -4,7 +4,6 @@
 #getData('ISO3')
 #getData('alt', country= 'USA', mask=T)
 
-
 #load libraries:
 library(lattice)
 library(geodata)
@@ -18,7 +17,20 @@ library(rasterVis)
 
 #this will get the DEM for the whole USA:
 rastertest <- raster('USA1_msk_alt.gri')
+
+#crop to bounding box:
+bounds <- read.csv("2022_03_29_latlonboundingbox.csv")
+extents <- c(round(min(bounds$x)),
+             round(max(bounds$x)),
+             round(min(bounds$y)),
+             round(max(bounds$y)))
+box <- as(extent(extents), 'SpatialPolygons')
+crs(box) <- "+proj=longlat +datum=WGS84 +no_defs"
+#rastbox <- crop(rastertest, box)
+
+rastertestproj <- projectRaster(rastertest, "+proj=longlat +datum=WGS84 +no_defs")
 #plot(rastertest)
+
 
 
 
