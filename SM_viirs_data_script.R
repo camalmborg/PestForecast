cf4d1c1bcb16ae478cc8eb20673c0232bef0f064
camalmborg <malmborg@bu.edu>
2023-05-17 14:34:33 -0400

    first commit viirs data script

---
+++ SM_viirs_data_script.R
@@ -0,0 +1,10 @@
+### script for VIIRS download and processing
+
+### Attempt to use opendapr package----------------------------------
+
+#install packages:
+# library(devtools)
+# devtools::install_github("ptaconet/opendapr")
+## Online it says "still in development" -- wasn't able to get it to install properly
+
+##going to try extracting from GEE
