e6830b9f7ec60dc0dfafdc0006b61f3d2d3f9f57
camalmborg <malmborg@bu.edu>
2023-05-23 12:17:04 -0400

    setting up univariate analyses. Raster package is deprecated, will have to move code to terra in future

--- SM_dem_slope_aspect_script.R
+++ SM_dem_slope_aspect_script.R
@@ -9,8 +9,9 @@ library(lattice)
 library(geodata)
 library(rgdal)
 library(rgeos)
-library(raster)
-library(rasterVis)
+library(terra)
+#library(raster)  ##deprecated
+#library(rasterVis)
 
 #url for DEM data:
 #url <- "http://doi.org/10.5067/MEaSUREs/SRTM/SRTMGL1N.003"
@@ -20,7 +21,7 @@ library(rasterVis)
 
 #this will get the DEM for the whole USA:
 #raster package becoming unavailable, replaced by terra
-rastertest <- raster('USA1_msk_alt.gri')
+rastertest <- raster('USA1_msk_alt.tif')
 
 #crop to bounding box:
 bounds <- read.csv("2022_03_29_latlonboundingbox.csv")
@@ -67,3 +68,39 @@ datastack <- stack(rastbox, slopes, aspects)
 DEMdata <- extract(datastack, sites)
 #save a csv and GEOTIFF of this -- 5/19
 
+
+### Univariate Analyses ------------------------------------------
+
+#load libraries:
+library(mgcv)
+
+#function for univariate analyses:
+DSA_explore<-function(dem,x,y,z){
+  #make empty matrix:
+  r2s <- matrix(NA,nrow=ncol(dem[[1]]),ncol=length(dmvars))
+  
+  #loop over all members of dmvars list:
+  for (i in 1:length(dmvars)){
+    #first extract the list you want:
+    dmvariable <- as.data.frame(dmvars[[i]][as.numeric(dmrdat$sitenum),])
+    #grab column number:
+    cn <- as.matrix(as.numeric(dmrdat$colnum))
+    #grab exploratory response  of choice:
+    y <- as.matrix(as.numeric(dmrdat[,dmr]))
+    x <- as.data.frame(cbind(y,cn,dmvariable))
+    vardat <- x[x$cn==coln,]
+    
+    #loop for filling in R2 table:  
+    for (j in 1:ncol(dmvariable)){
+      var.gam <- gam(vardat[,1]~s(vardat[,j+2]),data=vardat)
+      
+      #extract r2:
+      summ <- summary(var.gam)
+      r2s[j,i] <-summ$r.sq
+    }
+  }
+  # return(vardat)
+  return(r2s)
+}
+
+testing <- dm_explore(spongyvars,testfx,"mags",22)
