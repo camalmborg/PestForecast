b896f3b0223db01d853ad7e6af29ba308b93eb50
camalmborg <malmborg@bu.edu>
2023-06-06 12:20:15 -0400

    still an error in the loop somewhere

--- SM_disturb_mag_recov_rate_calc_script_CS_version.R
+++ SM_disturb_mag_recov_rate_calc_script_CS_version.R
@@ -16,66 +16,70 @@ tcg.values<-read.csv(file)
 cond.scores<-read.csv(cfile)
 
 #### Function for computing disturbance magnitudes, probabilities, and recovery rates:
-spongy_mpr<-function(tcg,cs,distyr){
+spongy_mpr_cs<-function(tcg,cs,distyr){
   #get just the tcg values from data frame:
   tcgs<-tcg[,c(grep("^X",colnames(tcg)))]
   sitenum<-as.matrix(1:nrow(tcgs))
   
+  #get just cs values from data frame:
+  css <- cs[,c(grep("^X",colnames(cs)))]
+  
   #first get just june values:
   junes<-seq(3,ncol(tcgs),by=5)  #3 is if tcg/condscores data have april in them, 2 if data starts w/May
-  tcgjune<-as.matrix(tcgs[,junes])
+  #tcgjune<-as.matrix(tcgs[,junes])
+  csjune <- as.matrix(css[,junes])
   
   #identify steady state (mean tcg previous 3 years):
-  prevyrs<-tcgjune[,grep(as.character(distyr-3),colnames(tcgjune)):
-                     grep(as.character(distyr-1),colnames(tcgjune))]
+  prevyrs<-csjune[,grep(as.character(distyr-3),colnames(csjune)):
+                     grep(as.character(distyr-1),colnames(csjune))]
   steadys<-as.matrix(apply(prevyrs[,1:3],1,mean,na.rm=T))
   
   #identify missing data columns:
-  NAy1<-which(is.na(tcgjune[,grep(as.character(distyr),colnames(tcgjune))]))
-  NAy2<-which(is.na(tcgjune[,grep(as.character(distyr+1),colnames(tcgjune))]))
+  NAy1<-which(is.na(csjune[,grep(as.character(distyr),colnames(csjune))]))
+  NAy2<-which(is.na(csjune[,grep(as.character(distyr+1),colnames(csjune))]))
   
   missing<-intersect(NAy1,NAy2) 
   
   if (length(missing) == 0){
-    tcgjune<-tcgjune
+    csjune<-csjune
     steadys<-steadys
   } else {
-    tcgjune<-tcgjune[-missing,]
+    csjune<-csjune[-missing,]
     steadys<-steadys[-missing]
   }
   
   ### THE CALCULATION LOOP:
-  recov.rate<-vector()
+  #recov.rate<-vector()
   slope<-list()
   mins<-vector()
   colnum<-vector()
-  recovcol<-vector()
-  end<-length(tcgjune[1,])
-  nsite<-length(tcgjune[,1])
+  #recovcol<-vector()
+  end<-length(csjune[1,])
+  nsite<-length(csjune[,1])
   #recov<-list()
   for (i in 1:nsite){
-    mins[i]<-min(tcgjune[i,grep(as.character(distyr),colnames(tcgjune)):
-                           grep(as.character(distyr+1),colnames(tcgjune))],na.rm=T)        #grabs min forest condition score for each site
-    colnum[i]<-which(tcgjune[i,grep(as.character(distyr),colnames(tcgjune)):
-                               grep(as.character(distyr+1),colnames(tcgjune))]==mins[i])           #grabs time step at which min score appears in disturbance window
-    colnum[i] <- ifelse(colnum[i]==1, (grep(as.character(distyr),colnames(tcgjune))),
-                        (grep(as.character(distyr),colnames(tcgjune))+1))
+    mins[i]<-min(csjune[i,grep(as.character(distyr),colnames(csjune)):
+                           grep(as.character(distyr+1),colnames(csjune))],na.rm=T)        #grabs min forest condition score for each site
+    colnum[i]<-which(csjune[i,grep(as.character(distyr),colnames(csjune)):
+                               grep(as.character(distyr+1),colnames(csjune))]==mins[i])           #grabs time step at which min score appears in disturbance window
+    colnum[i] <- ifelse(colnum[i]==1, (grep(as.character(distyr),colnames(csjune))),
+                        (grep(as.character(distyr),colnames(csjune))+1))
     
-    if(is.na(colnum[i] + which(tcgjune[i,colnum[i]:end]>=steadys[i])[1])){
-      recovcol[i]<-colnum[i] + 2 #why?
-    } else {
-      recovcol[i]<-colnum[i] + which(tcgjune[i,colnum[i]:end]>=steadys[i])[1]
-    }
-    
-    if(recovcol[i]>end){
-      recov<-tcgjune[i,colnum[i]:end]
-    } else {
-      recov<-tcgjune[i,colnum[i]:recovcol[i]]
-    }
-    # ##recov <- tcg[i,colnum[i]:recovcol[i]]
-    ind<-1:length(recov)                      #grabs length of recov rate
-    slope[[i]]<-lm(recov~ind)                      #runs the lm to find slope of recov rate, saves output
-    recov.rate[i]<-slope[[i]]$coefficients["ind"] #stores recovery rate
+    # if(is.na(colnum[i] + which(tcgjune[i,colnum[i]:end]>=steadys[i])[1])){
+    #   recovcol[i]<-colnum[i] + 2 #why?
+    # } else {
+    #   recovcol[i]<-colnum[i] + which(tcgjune[i,colnum[i]:end]>=steadys[i])[1]
+    # }
+    # 
+    # if(recovcol[i]>end){
+    #   recov<-tcgjune[i,colnum[i]:end]
+    # } else {
+    #   recov<-tcgjune[i,colnum[i]:recovcol[i]]
+    # }
+    # # ##recov <- tcg[i,colnum[i]:recovcol[i]]
+    # ind<-1:length(recov)                      #grabs length of recov rate
+    # slope[[i]]<-lm(recov~ind)                      #runs the lm to find slope of recov rate, saves output
+    # recov.rate[i]<-slope[[i]]$coefficients["ind"] #stores recovery rate
   }
   rm(slope)
   
@@ -86,17 +90,17 @@ spongy_mpr<-function(tcg,cs,distyr){
   magsdiv<-mags/steadys
   
   #calculate recovery time:
-  recov.time<-mags/recov.rate
+  #recov.time<-mags/recov.rate
   
   #combine data into dataframe:
   if (length(missing) == 0){
-    tcg.mx<-cbind(tcg[,1],sitenum,steadys,colnum,mins,mags,magsdiv,recov.rate,recov.time)
+    cs.mx<-cbind(css[,1],sitenum,steadys,colnum,mins,mags,magsdiv)#,recov.rate,recov.time)
   } else{
-    tcg.mx<-cbind(tcg[-missing,1],sitenum[-missing],steadys,colnum,mins,mags,magsdiv,recov.rate,recov.time)
+    cs.mx<-cbind(css[-missing,1],sitenum[-missing],steadys,colnum,mins,mags,magsdiv)#,recov.rate,recov.time)
   }
   
-  tcg.m<-as.data.frame(tcg.mx)
-  colnames(tcg.m)<-c("id","sitenum","steady","colnum","mins","mags","magsdiv","recov.rate","recov.time")
+  cs.m<-as.data.frame(cs.mx)
+  colnames(cs.m)<-c("id","sitenum","steady","colnum","mins","mags","magsdiv","recov.rate","recov.time")
   #return(tcg.m)
   
   ### Condition scores for disturbance probabilties
@@ -125,13 +129,11 @@ spongy_mpr<-function(tcg,cs,distyr){
   }
   
   #add disturbance probabilities to dataframe:
-  tcg.m$dpy1<-distprob[,1]
-  tcg.m$dpy2<-distprob[,2]
+  cs.m$dpy1<-distprob[,1]
+  cs.m$dpy2<-distprob[,2]
   
-  return(tcg.m)
+  return(cs.m)
 }
 
-testfx<-spongy_mpr(tcg.values,cond.scores,2016)
-
-#once again I am a beautiful genius!!!!
+testfx<-spongy_mpr_cs(tcg.values,cond.scores,2016)
 
