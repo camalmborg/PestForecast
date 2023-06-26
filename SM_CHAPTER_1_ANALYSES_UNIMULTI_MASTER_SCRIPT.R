095d5393077fb5729f8208d3041f4d878f756beb
camalmborg <malmborg@bu.edu>
2023-06-26 13:10:39 -0400

    adding list version of uni-analysis (daymet)

--- SM_CHAPTER_1_ANALYSES_UNIMULTI_MASTER_SCRIPT.R
+++ SM_CHAPTER_1_ANALYSES_UNIMULTI_MASTER_SCRIPT.R
@@ -7,3 +7,40 @@ library(mgcv)
 library(pROC)
 library(combinat)
 
+##### UNIVARIATE ANALYSES SECTION ------------------------------------------------
+
+### Function for Univariate Analyses AS LIST  (Daymet data):
+# dmvars = data in list - Daymet data (each variable is list member)
+# dmrdat = disturbance magnitude and probability data - object from DISTMAGRECOV 
+#          calculation script
+# dmr = which from disturbance probability, magnitude, or recovery to use for 
+#       analysis - "dpy1"/"dpy2" for 2016/2017 distprob, "mags" for magnitude
+# coln = column number of disturbance - 22 for 2016, 23 for 2017
+
+SM_l_explore<-function(dmvars,dmrdat,dmr,coln){
+  #make empty matrix:
+  r2s <- matrix(NA,nrow=ncol(dmvars[[1]]),ncol=length(dmvars))
+  
+  #loop over all members of dmvars list:
+  for (i in 1:length(dmvars)){
+    #first extract the list you want:
+    dmvariable <- as.data.frame(dmvars[[i]][as.numeric(dmrdat$sitenum),])
+    #grab column number:
+    cn <- as.matrix(as.numeric(dmrdat$colnum))
+    #grab exploratory response  of choice:
+    y <- as.matrix(as.numeric(dmrdat[,dmr]))
+    x <- as.data.frame(cbind(y,cn,dmvariable))
+    vardat <- x[x$cn==coln,]
+    
+    #loop for filling in R2 table:  
+    for (j in 1:ncol(dmvariable)){
+      var.gam <- gam(vardat[,1]~s(vardat[,j+2]),data=vardat)
+      
+      #extract r2:
+      summ <- summary(var.gam)
+      r2s[j,i] <-summ$r.sq
+    }
+  }
+  # return(vardat)
+  return(r2s)
+}
