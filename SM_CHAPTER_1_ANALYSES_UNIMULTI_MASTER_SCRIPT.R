750fef25a3efabb3c9488cad1a057d3746c6ca62
camalmborg <malmborg@bu.edu>
2023-06-26 13:03:39 -0400

    making master script for all uni and multivariate analyses Chapter 1 - first commit

---
+++ SM_CHAPTER_1_ANALYSES_UNIMULTI_MASTER_SCRIPT.R
@@ -0,0 +1,9 @@
+# This new script contains all the code for running both the univariate and
+# multivariate analyses for Chapter 1 - disturbance probabilities and magnitudes
+# This code will run for Daymet (section 1), SMAP, VIIRS, and DEM data.
+
+### load libraries:
+library(mgcv)
+library(pROC)
+library(combinat)
+
