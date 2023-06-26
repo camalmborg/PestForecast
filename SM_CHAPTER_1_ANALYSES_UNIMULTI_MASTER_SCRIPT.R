52772d66c930f492bd8320097fc9af3a564adc1e
camalmborg <malmborg@bu.edu>
2023-06-26 14:22:13 -0400

    disturbance probabilities sections added

--- SM_CHAPTER_1_ANALYSES_UNIMULTI_MASTER_SCRIPT.R
+++ SM_CHAPTER_1_ANALYSES_UNIMULTI_MASTER_SCRIPT.R
@@ -9,7 +9,7 @@ library(combinat)
 
 ##### UNIVARIATE ANALYSES SECTION ------------------------------------------------
 
-### Function for Univariate Analyses AS LIST  (Daymet data):----------------------
+### Function for Univariate mags Analyses AS LIST  (Daymet data):----------------------
 # dmvars = data in list - Daymet data (each variable is list member)
 # dmrdat = disturbance magnitude and probability data - object from DISTMAGRECOV 
 #          calculation script
@@ -45,7 +45,7 @@ SM_l_explore<-function(dmvars,dmrdat,dmr,coln){
   return(r2s)
 }
 
-### Function for univariate analysis NOT in list mode:----------------------------
+### Function for univariate mags analysis NOT in list mode:----------------------------
 # var = variable of interest (DEM, VIIRS, SMAP etc.)
 # dmrdat = disturbance magnitude and probability data - object from DISTMAGRECOV 
 #          calculation script
@@ -79,3 +79,164 @@ SM_var_explore<-function(var,dmrdat,dmr,coln){
   return(r2s)
 }
 
+### Functions for disturbance probability univariate analyses --------------------
+### List input version (Daymet):---------------------------------------------------
+# var = variable of interest (DEM, VIIRS, SMAP etc.)
+# dmrdat = disturbance magnitude and probability data - object from DISTMAGRECOV 
+#          calculation script
+# yr = year disturbance takes place - 1/2 for 2016/2017 respectively
+# coln = column number of disturbance - 22 for 2016, 23 for 2017
+
+spongy_lv_ROC <- function(dmvars,dmrdat,yr,coln){
+  #make empty matrix:
+  rocs <- matrix(NA,nrow=ncol(dmvars[[1]]),ncol=length(dmvars))
+  
+  #grab just disturbance probability columns:
+  dists<-dmrdat[,grep("^dp",colnames(dmrdat))]
+  
+  #loop over all members of dmvars list:
+  for (i in 1:length(dmvars)){
+    #first extract the list you want:
+    dmvariable <- as.data.frame(dmvars[[i]][as.numeric(dmrdat$sitenum),])
+    #grab column number:
+    cn <- as.matrix(as.numeric(dmrdat$colnum))
+    #grab exploratory response  of choice:
+    y <- as.matrix(as.numeric(dists[,yr]))
+    x <- as.data.frame(cbind(y,cn,dmvariable))
+    vardat <- x[x$cn==coln,]
+    
+    #loop for filling in R2 table:  
+    for (j in 1:ncol(dmvariable)){
+      var.gam<-gam(vardat[,1]~s(vardat[,j+2]), data=vardat, family="binomial")
+      var.roc<-roc(vardat[,1],var.gam$fitted.values)
+      rocs[j,i] <-var.roc$auc
+    }
+  }
+  #return table of AUCs
+  return(rocs)
+  
+}
+
+### Matrix input version: ---------------------------------------------------------
+spongy_var_ROC <- function(var,dmrdat,yr,coln){
+  #make empty matrix:
+  rocs <- matrix(NA,nrow=ncol(var),ncol=1)  #hard coded rn
+  
+  #grab just disturbance probability columns:
+  dists<-dmrdat[,grep("^dp",colnames(dmrdat))]
+  
+  #first get data:
+  vvar <- as.data.frame(var[as.numeric(dmrdat$sitenum),])
+  #grab column number:
+  cn <- as.matrix(as.numeric(dmrdat$colnum))
+  #grab exploratory response  of choice:
+  y <- as.matrix(as.numeric(dists[,yr]))
+  x <- as.data.frame(cbind(y,cn,vvar))
+  vardat <- x[x$cn==coln,]
+  
+  #loop over all members of dmvars:
+  for (i in 1:ncol(var)){  #this is hard coded until further notice
+    #loop for filling in R2 table:  
+    var.gam<-gam(vardat[,1]~s(vardat[,i+2]), data=vardat, family="binomial")
+    var.roc<-roc(vardat[,1],var.gam$fitted.values)
+    rocs[i,] <-var.roc$auc
+  }
+  #return table of AUCs
+  return(rocs)
+}
+
+### MULTIVARIATE ANALYSES SECTION ------------------------------------------------
+
+### Function for Multivariate disturbance magnitude analyses:---------------------
+# data = explanatory variables of interest matrix/dataframe
+# nvars = number of variables in each model run (numeric)
+# dmrdat = disturbance magnitude and probability data - object from DISTMAGRECOV 
+#          calculation script
+
+SM_multi_var <- function(data, nvars, dmrdat) {
+  #make combinations of nvars variables:
+  combi <- t(combn(ncol(data),nvars))
+  
+  #make empty matrix:
+  r2s <- matrix(nrow=nrow(combi), ncol=2) #need to know dims
+  r2s[,1] <- 1:nrow(combi)
+  
+  aics <- vector()
+  
+  #vardattest <- list()
+  #vardat loop:
+  for (i in 1:nrow(combi)){
+    #make gam variables data frame:
+    vardat <- as.data.frame(cbind(dmrdat$mags, data[,c(combi[i,])]))
+    
+    #make gam explantory variables list
+    ex_vars <- c()
+    for (j in 1:nvars){
+      ex_vars[j] <- paste0('s(vardat[,', j+1, '])')
+    }
+    
+    #make a single string:
+    gam_formula <- as.formula(paste("vardat[,1] ~ ",
+                                    paste(ex_vars, collapse='+')))
+    
+    #run gam with those data:
+    mv_gam <- gam(gam_formula)
+    summ <- summary(mv_gam)
+    r2s[i,2] <- summ$r.sq
+    aics[i] <- mv_gam$aic
+  }
+  
+  delta <- min(aics)
+  delAIC <- aics-delta
+  models <- cbind(r2s, combi, delAIC)
+  return(models)
+}
+
+
+### Function for Multivariate disturbance probability analyses:-------------------
+# data = explanatory variables of interest matrix/dataframe
+# nvars = number of variables in each model run (numeric)
+# dmrdat = disturbance magnitude and probability data - object from DISTMAGRECOV 
+#          calculation script
+# yr = disturbance year (1 = 2016; 2 = 2017)
+SM_multi_ROC <- function(data,nvars,dmrdat,yr){
+  #make combinations of nvars variables:
+  combi <- t(combn(ncol(data),nvars))
+  
+  #make empty matrix:
+  rocs <- matrix(nrow=nrow(combi), ncol=2) #need to know dims
+  rocs[,1] <- 1:nrow(combi)
+  
+  aics <- vector()
+  
+  #grab just disturbance probability columns:
+  dists<-dmrdat[,grep("^dp",colnames(dmrdat))]
+  
+  #vardat loop:
+  for (i in 1:nrow(combi)){
+    #make gam variables data frame:
+    vardat <- as.data.frame(cbind(dists[,yr], data[,c(combi[i,])]))
+    
+    #make gam explantory variables list
+    ex_vars <- c()
+    for (j in 1:nvars){
+      ex_vars[j] <- paste0('s(vardat[,', j+1, '])')
+    }
+    
+    #make a single string:
+    gam_formula <- as.formula(paste("vardat[,1] ~ ",
+                                    paste(ex_vars, collapse='+')))
+    
+    #run gam with those data:
+    mv_gam <- gam(gam_formula, data=vardat, family="binomial")
+    mv_roc <- roc(vardat[,1], mv_gam$fitted.values)
+    rocs[i,2] <- mv_roc$auc
+    aics[i] <- mv_gam$aic
+  }
+  
+  delta <- min(aics)
+  delAIC <- aics-delta
+  models <- cbind(rocs, combi, delAIC)
+  return(models)
+  
+}
