53d360f235e51743fdfbb994d5154124e1fe5f36
camalmborg <malmborg@bu.edu>
2023-07-13 17:50:43 -0400

    trying to re-run hf data, doesn't work for some reason

--- SM_DISTURB_MAG_RECOV_RATE_CALC_SCRIPT.R
+++ SM_DISTURB_MAG_RECOV_RATE_CALC_SCRIPT.R
@@ -134,5 +134,6 @@ spongy_mpr<-function(tcg,cs,distyr){
 testfx<-spongy_mpr(tcg.values,cond.scores,2016)
 testfx2 <- spongy_mpr(cond.scores, cond.scores, 2016)
 
+hf_mags_2 <- spongy_mpr(tcg.values, cond.scores, 2017)
 #once again I am a beautiful genius!!!!
 
