53ab651c5a9fc4711dd8d5ec3d8d809dd986481a
camalmborg <malmborg@bu.edu>
2023-07-20 11:05:33 -0400

    saving figures script pulling interesting sites for example time series

--- 2023_ESA_Figures_Script.R
+++ 2023_ESA_Figures_Script.R
@@ -12,9 +12,27 @@ tcgs<-tcg[,c(grep("^X",colnames(tcg)))]
 #junes<-seq(3,ncol(tcgs),by=5)  #3 is if tcg/condscores data have april in them, 2 if data starts w/May
 #tcgjune<-as.matrix(tcgs[,junes])
 
-y <- tcgs[site, 31:79]
+y <- tcgs[site, 21:79]
 x <- length(y[1,])
 #samp_time_series <- tcgjune[site,]
 #par(mar=c(1,1,1,1))
 plot(1:x, y[1,])
 
+distsites <- c(10,18,21,30,31,32,47,53,60,
+               66,72,97,98,101,102,105,110,
+               111,120,130,135,141,142,145,
+               146,151,152,153,157,160,170,
+               171,172,188,195,196,203)
+distsdoub <- c(14,119,147,161,164,166,167,173,174,175,191,197)
+fastrecov <- c(41,42,43,94,95)
+slowrecov <- c(48,54,55,58,75,87)
+
+ds <- tcgs[distsites, 21:79]
+dd <- tcgs[distsdoub, 21:79]
+fr <- tcgs[fastrecov, 21:79]
+sr <- tcgs[slowrecov, 21:79]
+
+ds_mr <- hf_data[distsites,]
+dd_mr <- hf_data[distsdoub,]
+fr_mr <- hf_data[fastrecov,]
+sr_mr <- hf_data[slowrecov,]
