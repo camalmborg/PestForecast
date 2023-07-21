2be94a0ac3622b1d3579b45accd062cbd081c6da
camalmborg <malmborg@bu.edu>
2023-07-21 13:35:43 -0400

    setting up to make mortality plots

--- 2023_ESA_Figures_Script.R
+++ 2023_ESA_Figures_Script.R
@@ -4,7 +4,14 @@
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
+library(mgcv)
+library(pROC)
 
+### MAKING THE ROC PLOTS FOR MORTALITY:
+
+
+
+### MAKING SAMPLE TIME SERIES PLOTS:
 
 # Harvard Forest data - Landsat time series at some of the sites:
 site <- 166
@@ -16,17 +23,18 @@ tcgs<-tcg[,c(grep("^X",colnames(tcg)))]
 
 y <- as.numeric((tcgs[site,32:80])/1000) #currently Apr 2011-Aug 2020
 x <- 1:length(y)
-#samp_time_series <- tcgjune[site,]
-#par(mar=c(1,1,1,1))
-plot(x, y, frame=FALSE,
-     xlab = "Growing Season Month",
-     ylab = "Condition Score",
-     pch = 20,
-     col = "forestgreen")#,
-     #ylim = c(-6.75,4.5))
-#lines(x, y, lwd = 1, col="forestgreen")
 
- 
+# R PLOT VERSION:---------------------
+# #samp_time_series <- tcgjune[site,]
+# #par(mar=c(1,1,1,1))
+# plot(x, y, frame=FALSE,
+#      xlab = "Growing Season Month",
+#      ylab = "Condition Score",
+#      pch = 20,
+#      col = "forestgreen")#,
+#      #ylim = c(-6.75,4.5))
+# #lines(x, y, lwd = 1, col="forestgreen")
+
 sample_site <- data.frame(
   month = x,
   score = y
@@ -55,24 +63,25 @@ ggplot(data = sample_site, mapping = aes(x = x, y = score)) +
   #       panel.grid.minor = element_blank())
 #dev.off()
 
+
 #testing subset sites:
-distsites <- c(10,18,21,30,31,32,47,53,60,
-               66,72,97,98,101,102,105,110,
-               111,120,130,135,141,142,145,
-               146,151,152,153,157,160,170,
-               171,172,188,195,196,203)
-distsdoub <- c(14,119,147,161,164,166,167,173,174,175,191,197)
-fastrecov <- c(41,42,43,94,95)
-slowrecov <- c(48,54,55,58,75,87)
-
-ds <- tcgs[distsites, 21:79]
-dd <- tcgs[distsdoub, 21:79]
-fr <- tcgs[fastrecov, 21:79]
-sr <- tcgs[slowrecov, 21:79]
-
-ds_mr <- hf_data[distsites,]
-dd_mr <- hf_data[distsdoub,]
-fr_mr <- hf_data[fastrecov,]
-sr_mr <- hf_data[slowrecov,]
+# distsites <- c(10,18,21,30,31,32,47,53,60,
+#                66,72,97,98,101,102,105,110,
+#                111,120,130,135,141,142,145,
+#                146,151,152,153,157,160,170,
+#                171,172,188,195,196,203)
+# distsdoub <- c(14,119,147,161,164,166,167,173,174,175,191,197)
+# fastrecov <- c(41,42,43,94,95)
+# slowrecov <- c(48,54,55,58,75,87)
+# 
+# ds <- tcgs[distsites, 21:79]
+# dd <- tcgs[distsdoub, 21:79]
+# fr <- tcgs[fastrecov, 21:79]
+# sr <- tcgs[slowrecov, 21:79]
+# 
+# ds_mr <- hf_data[distsites,]
+# dd_mr <- hf_data[distsdoub,]
+# fr_mr <- hf_data[fastrecov,]
+# sr_mr <- hf_data[slowrecov,]
 
 
