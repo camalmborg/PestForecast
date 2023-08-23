96c6afd53282ac4292aac9bed4a6f00033ab9f6b
camalmborg <malmborg@bu.edu>
2023-08-23 12:03:45 -0400

    making uni and multi into one big dataframe for identifying best models

--- SM_CHAPTER_1_MODEL_PERFORMANCE_SCRIPT.R
+++ SM_CHAPTER_1_MODEL_PERFORMANCE_SCRIPT.R
@@ -1,12 +1,41 @@
 # This is the script for fucking around and finding out the best performing models
 # CHAPTER 1 BEST MODELS FOR JAGS HINDCASTS
 
-#loading the model results masterlist files:
+# libraries:
+library(dplyr)
+library(tidyverse)
+
+# loading the model results masterlist files:
 distmag_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTMAG-UNI-GAM-ANALYSES-TCG-MASTERLIST.csv")
 distmag_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTMAG-MULTI-GAM-ANALYSES-TCG-MASTERLIST.csv")
-#distprob16_uni <-
-#distprob16_multi <-
-#distprob17_uni <- 
-#distprob17_multi <-
+#distprob16_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2016-UNI-AUC-ANALYSES-TCG-MASTERLIST.csv")
+#distprob16_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2016-MULTI-AUC-ANALYSES-TCG-MASTERLIST.csv")
+#distprob17_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2017-UNI-AUC-ANALYSES-TCG-MASTERLIST.csv")
+#distprob17_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2017-MULTI-AUC-ANALYSES-TCG-MASTERLIST.csv")
 
-#
+### Making uni and multi into something we can Rbind
+# choose group you would like to use from above:
+uni <- distmag_uni
+multi <- distmag_multi
+
+# univariate variable and monthyear columns combine:
+uni <- uni %>%
+  mutate(MONTHYEAR = str_replace(MONTHYEAR, " ", "-")) %>%
+  mutate(VARIABLE1 = paste(VARIABLE1, MONTHYEAR, sep="_")) %>%
+  select(-c(MONTHYEAR))
+
+# make new dataframe with 2 new NA columsn for VARIABLE2 and VARIABLE3:
+v2v3 <- data.frame(matrix(nrow=nrow(uni), ncol=2, data=NA))
+colnames(v2v3) <- c("VARIABLE2","VARIABLE3")
+# combine:
+uni <- as.data.frame(cbind(uni$MODEL_ID,uni$VARIABLE1,
+                                   v2v3$VARIABLE2,v2v3$VARIABLE3,
+                                   uni$R2, uni$AIC))
+colnames(uni) <- names(multi)
+
+# Combining them into one big dataframe:
+model <- rbind(uni, multi)
+
+
+### Choosing best performers:
+
