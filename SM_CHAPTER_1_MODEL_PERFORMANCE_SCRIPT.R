9b0ec0f49604cb5ed5ef3b808adfa81664a02257
camalmborg <malmborg@bu.edu>
2023-08-23 11:27:43 -0400

    CH1 model performance assessment script

---
+++ SM_CHAPTER_1_MODEL_PERFORMANCE_SCRIPT.R
@@ -0,0 +1,12 @@
+# This is the script for fucking around and finding out the best performing models
+# CHAPTER 1 BEST MODELS FOR JAGS HINDCASTS
+
+#loading the model results masterlist files:
+distmag_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTMAG-UNI-GAM-ANALYSES-TCG-MASTERLIST.csv")
+distmag_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTMAG-MULTI-GAM-ANALYSES-TCG-MASTERLIST.csv")
+#distprob16_uni <-
+#distprob16_multi <-
+#distprob17_uni <- 
+#distprob17_multi <-
+
+#
