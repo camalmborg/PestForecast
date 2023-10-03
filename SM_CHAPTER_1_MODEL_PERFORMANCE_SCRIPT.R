b3fd26030420f7305e922485e1996d3f0126d432
camalmborg <malmborg@bu.edu>
2023-10-03 12:01:27 -0400

    biological best models ID

--- SM_CHAPTER_1_MODEL_PERFORMANCE_SCRIPT.R
+++ SM_CHAPTER_1_MODEL_PERFORMANCE_SCRIPT.R
@@ -6,38 +6,29 @@ library(dplyr)
 library(tidyverse)
 
 # loading the model results masterlist files:
-#distmag_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTMAG-UNI-GAM-ANALYSES-TCG-MASTERLIST.csv")
-#distmag_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTMAG-MULTI-GAM-ANALYSES-TCG-MASTERLIST.csv")
-#distprob16_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2016-UNI-AUC-ANALYSES-TCG-MASTERLIST.csv")
-#distprob16_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2016-MULTI-AUC-ANALYSES-TCG-MASTERLIST.csv")
-#distprob17_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2017-UNI-AUC-ANALYSES-TCG-MASTERLIST.csv")
-#distprob17_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2017-MULTI-AUC-ANALYSES-TCG-MASTERLIST.csv")
 
-#distmag_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTMAG-UNI-GAM-ANALYSES-CS-MASTERLIST.csv")
-#distmag_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTMAG-MULTI-GAM-ANALYSES-CS-MASTERLIST.csv")
-#distprob16_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2016-UNI-AUC-ANALYSES-CS-MASTERLIST.csv")
-#distprob16_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2016-MULTI-AUC-ANALYSES-CS-MASTERLIST.csv")
-#distprob17_uni <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2017-UNI-AUC-ANALYSES-CS-MASTERLIST.csv")
-#distprob17_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB2017-MULTI-AUC-ANALYSES-CS-MASTERLIST.csv")
+# distmag_uni <- read.csv("CHAPTER_1/2023_September/2023_09_UNI_Dist_Mag_TCG_2016_Models_R2s_AICs.csv")
+# distmagcs_uni <- read.csv("CHAPTER_1/2023_September/2023_09_UNI_Dist_Mag_CS_2016_Models_R2s_AICs.csv")
+# distmag_multi <- read.csv("CHAPTER_1/2023_September/CHAPTER 1-DISTMAG2016-MULTI-GAM-ANALYSES-TCG-MASTERLIST.csv")
+# distmagcs_multi <- read.csv("CHAPTER_1/2023_September/CHAPTER 1-DISTMAG2016- MULTI-GAM-ANALYSES-CS-MASTERLIST.csv")
 
-distmag_uni <- read.csv("CHAPTER_1/2023_September/2023_09_UNI_Dist_Mag_TCG_2016_Models_R2s_AICs.csv")
-distmagcs_uni <- read.csv("CHAPTER_1/2023_September/2023_09_UNI_Dist_Mag_CS_2016_Models_R2s_AICs.csv")
-distmag_multi <- read.csv("CHAPTER_1/2023_September/CHAPTER 1-DISTMAG2016-MULTI-GAM-ANALYSES-TCG-MASTERLIST.csv")
-distmagcs_multi <- read.csv("CHAPTER_1/2023_September/CHAPTER 1-DISTMAG2016- MULTI-GAM-ANALYSES-CS-MASTERLIST.csv")
-
-#distprob_uni <- read.csv("Analyses_September2023/2023_09_UNI_Dist_Prob_TCG_2016_Models_ROCs_AICs.csv")
-#distprob_uni <- read.csv("Analyses_September2023/2023_09_UNI_Dist_Prob_TCG_2017_Models_ROCs_AICs.csv")
-#distprob_uni <- read.csv("Analyses_September2023/2023_09_UNI_Dist_Prob_CS_2016_Models_ROCs_AICs.csv")
-#distprob_uni <- read.csv("Analyses_September2023/2023_09_UNI_Dist_Prob_CS_2017_Models_ROCs_AICs.csv")
-
-
+# distprob16tcg_uni <-read.csv()
+# distprob16cs_uni <- read.csv()
+# distprob17tcg_uni <- read.csv()
+# distprob17cs_uni <- read.csv()
+  
+distprob16cs_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB-2016-MULTI-AUC-ANALYSES-CS-MASTERLIST.csv")
+distprob16tcg_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB-2016-MULTI-AUC-ANALYSES-TCG-MASTERLIST.csv")
+distprob17cs_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB-2017-MULTI-AUC-ANALYSES-CS-MASTERLIST.csv")
+distprob17tcg_multi <- read.csv("CHAPTER_1/CHAPTER 1-DISTPROB-2017-MULTI-AUC-ANALYSES-TCG-MASTERLIST.csv")
+  
 ### Making uni and multi into something we can Rbind
 # choose group you would like to use from above:
-#uni <- distmag_uni
-#multi <- distmag_multi
 
-uni <- distmagcs_uni
-multi <- distmagcs_multi
+# uni <- distmagcs_uni
+# multi <- distmagcs_multi
+
+multi <- distprob16cs_multi
 
 
 # DIST MAG: univariate variable and monthyear columns combine: -------------
@@ -80,14 +71,44 @@ model <- rbind(uni, multi)
 
 ### Calculate delAIC: -----------
 model$delAIC <- model$AIC - min(model$AIC)
+model$delAIC <- model$aics - min(model$aics)
 
 ### Choosing best performers:
 modAIC <- model[order(model$delAIC),]
-modr2 <- model[order(model$R2),]
-#modauc <- model[order(model$AUC),]
+#modr2 <- model[order(model$R2),]
+modauc <- model[order(model$AUC),]
 #modauc <- model[order(model$ROC),]
 
 write.csv(modAIC, file="CHAPTER_1/2023_09_29_DISTMAG_2016_CS_delAICsort.csv")
 write.csv(modr2, file="CHAPTER_1/2023_09_29_DISTMAG_2016_CS_r2sort.csv")
 #write.csv(modAIC, file="CHAPTER_1/dist_prob_2017_cs_models_AICs_sorted.csv")
 #write.csv(modauc, file="CHAPTER_1/dist_prob_2017_cs_models_auc_sorted.csv")
+
+
+##### BIOLOGICAL PLAUSIBILITY 10/03/2023:
+distmag_vars <- c(2, 6, 7, 11, 12, 13, 14, 15, 16, 17,
+                  22, 23, 24, 25, 31, 32, 33)
+distprob_vars <- c( 2, 7, 8, 9, 10, 11, 12,
+                    18, 19, 20, 21, 22, 23, 24, 25,
+                    32, 33, 34, 35, 38, 39, 40)
+
+dm_bio_best_tcg <- best_tcg[best_tcg$VARIABLE1 %in% distmag_vars &
+                       best_tcg$VARIABLE2 %in% distmag_vars &
+                       best_tcg$VARIABLE3 %in% distmag_vars,]
+dm_bio_best_cs <- best_cs[best_cs$VARIABLE1 %in% distmag_vars &
+                             best_cs$VARIABLE2 %in% distmag_vars &
+                             best_cs$VARIABLE3 %in% distmag_vars,]
+
+dp16_bio_best_tcg <- distprob16tcg_multi[distprob16tcg_multi$VARIABLE1 %in% distprob_vars &
+                                           distprob16tcg_multi$VARIABLE2 %in% distprob_vars &
+                                           distprob16tcg_multi$VARIABLE3 %in% distprob_vars,]
+dp16_bio_best_cs <- distprob16cs_multi[distprob16cs_multi$VARIABLE1 %in% distprob_vars &
+                                          distprob16cs_multi$VARIABLE2 %in% distprob_vars &
+                                          distprob16cs_multi$VARIABLE3 %in% distprob_vars,]
+
+dp17_bio_best_tcg <- distprob17tcg_multi[distprob17tcg_multi$VARIABLE1 %in% distprob_vars &
+                                           distprob17tcg_multi$VARIABLE2 %in% distprob_vars &
+                                           distprob17tcg_multi$VARIABLE3 %in% distprob_vars,]
+dp17_bio_best_cs <- distprob16cs_multi[distprob17cs_multi$VARIABLE1 %in% distprob_vars &
+                                         distprob17cs_multi$VARIABLE2 %in% distprob_vars &
+                                         distprob17cs_multi$VARIABLE3 %in% distprob_vars,]
