d0a1810d7bc95cf956fc99e6065008b03deb78a0
camalmborg <malmborg@bu.edu>
2024-02-20 13:43:14 -0500

    first commit - code for making matrices of models for JAGS

---
+++ SM_Chapter_1_JAGS_model_matrix_maker.R
@@ -0,0 +1,16 @@
+# CHAPTER 1
+# this is a script for making matrix of models for disturbance magnitude and 
+# disturbance probability parameterizations
+
+### LIBRARIES:
+
+### LOAD DATA:
+# the environmental variables
+# for disturbance magnitude parameters
+varfile_m <- "CHAPTER_1/DATA/MV_2023_12_DATA_distmag.csv"
+magvars <- read.csv(varfile_m)[,-1]
+# for disturbance probability parameters
+#varfile_p <- "CHAPTER_1/DATA/MV_2023_12_DATA_distprob.csv"
+#probvars <- read.csv(varfile_p)[,-1]
+
+
