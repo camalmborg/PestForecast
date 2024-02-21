427aac5702876776d8eeee8f8fb3bed22955c76a
camalmborg <malmborg@bu.edu>
2024-02-21 10:49:20 -0500

    First commit - JAGS code diagnostics for Ch1

---
+++ SM_Chapter_1_JAGS_model_diagnostics_script.R
@@ -0,0 +1,11 @@
+# a scripte for viewing MCMC outputs and diagnostics
+
+### THE LIBRARIES:
+library(tidyverse)
+library(dplyr)
+library(rjags)
+library(coda)
+library(MCMCvis)
+#library(ecoforecastR)
+
+### 
