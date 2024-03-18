8a3b49b5ffee68468933712d8ac3d7029e6866fa
camalmborg <malmborg@bu.edu>
2024-03-18 12:00:04 -0400

    fixing jags model code

--- SM_JAGS_Model_Script.R
+++ SM_JAGS_Model_Script.R
@@ -238,31 +238,34 @@ spongy_disturb <- "model{
 for (s in 1:ns){
 
   #### Data Model:
-  y[s] ~ dnorm(mu[s], tau_obs[s])       ##3/1 - when adding precisions >> tau_obs[s]
+  y[s] ~ dnorm(mu[s], tau_obs[s])             ##3/1 - when adding precisions >> tau_obs[s]
   
   #### Process Model:
-  muN[s] <- R * x[s]                    ##step 3: dealing with modeling R (Chap 2 - RECOV)
+  muN[s] ~ dnorm(mun, pan)                    ##step 3: dealing with modeling R (Chap 2 - RECOV)
   ##x[s] ~ dnorm(mu[s], tau_add)
-  muD[s] ~ dnorm(mu0[s], pa0)           ##step 1: process model on mu0 (MAG)
+  muD[s] ~ dnorm(mu0[s], pa0)                 ##step 1: process model on mu0 (MAG)
     
-  ##D[s] ~ dbern(p)                     ##step 2: adding process model here (PROB)
+  ##D[s] ~ dbern(p)                           ##step 2: adding process model here (PROB)
   logit(D[s]) <- alpha0 + (alpha[1]*z[s])
     
   mu[s] <- D[s] * muD[s] + (1-D[s]) * muN[s]
   mu0[s] <- beta0 + (beta[1] * v[s]) ##+ (beta[2] * va[s]) + (beta[3] * vb[s])
+  mun <- R * x[s]
 
-  x[s] ~ dnorm(x_ic[s], tau_ic[s]) ##3/1 - this is where you would put previous timepoint
+  x[s] ~ dnorm(x_ic[s], tau_ic[s])            ##x[s] as distribution of prior timepoint and prior timepoint precision
   
 }#end loop over sites
   
   #### Priors
-  #tau_obs ~ dgamma(t_obs, a_obs)     ##observation error (data model)
-  #tau_add ~ dgamma(a_add ,t_add)     ##process error (process model)
-  R ~ dnorm(rmean, rprec)             ##rho paramter (recovery rate)
-  #p ~ dunif(0,1)                     ##disturbance probability
-  beta0 ~ dnorm(-5,1)                 ##param for calculating mean of disturbed state
-  alpha0 ~ dnorm(0, 0.0001) 
-  pa0 ~ dgamma(1,1) #precision of disturbed state
+  ##tau_obs ~ dgamma(t_obs, a_obs)     ##observation error (data model)
+  ##tau_add ~ dgamma(a_add ,t_add)     ##process error (process model)
+  ##p ~ dunif(0,1)                     ##disturbance probability
+  R ~ dnorm(rmean, rprec)              ##rho paramter (recovery rate)
+  pa0 ~ dgamma(1,1)                    ##precision of disturbed state
+  pan ~ dgamma(0,0.001)                ##precision of undisturbed state
+  beta0 ~ dnorm(-5,1)                  ##param for dist mag intercept
+  alpha0 ~ dnorm(0, 0.0001)            ##param for dist prob intercept
+
   
   ## COVARIATES WILL BE ADDED HERE
   beta[1] ~ dnorm(0, 0.0001)  
