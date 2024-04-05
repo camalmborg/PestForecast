30747a1f9d41823a593676cc3b5e10755d2157a3
camalmborg <malmborg@bu.edu>
2024-04-05 10:50:37 -0400

    update alpha version with correct cs data

--- SM_CHAPTER_1_JAGS_SCRIPT_for_SCC_ALPHA_VERSION.R
+++ SM_CHAPTER_1_JAGS_SCRIPT_for_SCC_ALPHA_VERSION.R
@@ -302,10 +302,12 @@ init <- list(R = R_mean,
 # prob covariates
 dpalpha <- dpls[[modelrun]]
 
+# condition score disturbance year onset data
+cs_model <- cs[grep(paste0("^", distyear, sep = ""), names(cs))]
 
 ### MODEL INPUTS
 # data object for model runs
-data = list(y = cs_dists, ns = nsites,
+data = list(y = cs_model, ns = nsites,
             x_ic = xic, tau_ic = tic,
             tau_obs = cs_precs,
             a = dmalpha,
