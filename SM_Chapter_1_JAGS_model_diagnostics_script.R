d6824e0bf911a209dba0195560e7ad00156ee46e
camalmborg <malmborg@bu.edu>
2024-07-01 11:28:50 -0400

    running for final model records and EFI conference plots

--- SM_Chapter_1_JAGS_model_diagnostics_script.R
+++ SM_Chapter_1_JAGS_model_diagnostics_script.R
@@ -9,23 +9,30 @@ library(MCMCvis)
 library(ecoforecastR)
 
 
-### OUT object:
-# model name
-jagmod = j.pests
-# iterations and thinning
-niter = 100000
-jthin = 1
-# variable names object
-jagvars <- c("beta0", "alpha0",
-             "beta[1]", "alpha[1]",
-             "R",
-             "pa0")
+### FROM SCC FILES:
+# load model outputs
+runpath <- "CHAPTER_1/2024_JAGS_models/Best_Models_from_SCC/model_runs/"
+outpath <- "CHAPTER_1/2024_JAGS_models/Best_Models_from_SCC/model_outputs/"
+# model data
+#runfile <- "A_best_alpha/2024-05-12_modelrun_alpha_a_2_b_1_data.RData"
+#runfile <- "A_best_beta/2024-05-06_modelrun_beta_a_1_b_15_data.RData"
+runfile <- "2024-06-25_modelrun_joint_a_4_b_6_data.RData"
+run <- load(paste0(runpath, runfile))
+# model output
+#outfile <- "A_best_alpha/2024-05-16_modelrun_alpha_a_1_b_1_output.csv"
+#outfile <- "A_best_beta/2024-05-16_modelrun_beta_a_1_b_4_output.csv"
+#outfile <- "2024-06-19_modelrun_joint_a_1_b_6_output.csv"
+#out <- read.csv(paste0(outpath, outfile))
 
-# run from model in SM_JAGS_Model_script
-jpout<-coda.samples(jagmod,
-                    variable.names = jagvars,
-                    n.iter = niter,
-                    thin = jthin)
+# data from model inputs for computing mu
+data <- model_info$metadata$data
+# covariates
+enva <- data$a
+envb <- data$b
+# DIC
+dic <- model_info$dic
+# JAGS output 
+jpout <- model_info$jpout
 
 ### Let's do some diagnostics:
 # plot trace and density plots
@@ -39,14 +46,14 @@ gelman.plot(jpout)
 
 # discarding burn in
 # set burn in based on GBR
-burnin <- 15000
+burnin <- 100000
 # remove burn in
 jburn <- window(jpout, start = burnin)
 # plot burn in output
 plot(jburn)
 
 # autocorrelation plots
-acfplot(jburn)  # ask Mike about this one
+#acfplot(jburn)  # ask Mike about this one
 
 # effective sample size
 effectiveSize(jburn)
@@ -65,8 +72,8 @@ cor(out)
 
 ### Model selection step:
 # DIC
-DIC <- dic.samples(j.pests, n,iter = 10000)
-sum <- sum(DIC$deviance, DIC$penalty)
+# DIC <- dic.samples(j.pests, n,iter = 10000)
+# sum <- sum(DIC$deviance, DIC$penalty)
 
 
 
@@ -87,3 +94,22 @@ parse.MatrixNames <- function(w, pre = "x", numeric = FALSE) {
   colnames(w) <- c("row", "col")
   return(as.data.frame(w))
 }
+
+### ARCHIVE::::
+# ### OUT object:
+# # model name
+# jagmod = j.pests
+# # iterations and thinning
+# niter = 100000
+# jthin = 1
+# # variable names object
+# jagvars <- c("beta0", "alpha0",
+#              "beta[1]", "alpha[1]",
+#              "R",
+#              "pa0")
+# 
+# # run from model in SM_JAGS_Model_script
+# jpout<-coda.samples(jagmod,
+#                     variable.names = jagvars,
+#                     n.iter = niter,
+#                     thin = jthin)
--- SM_Chapter_1_JAGS_model_output_script.R
+++ SM_Chapter_1_JAGS_model_output_script.R
@@ -8,7 +8,7 @@ library(readr)
 library(boot)
 library(MCMCvis)
 library(ecoforecastR)
-library(ggplot)
+library(ggplot2)
 library(knitr)
 
 ### load environmental data:
@@ -21,15 +21,18 @@ runpath <- "CHAPTER_1/2024_JAGS_models/Best_Models_from_SCC/model_runs/"
 outpath <- "CHAPTER_1/2024_JAGS_models/Best_Models_from_SCC/model_outputs/"
 # model data
 runfile <- "A_best_alpha/2024-05-10_modelrun_alpha_a_10_b_1_data.RData"
+#runfile <- "2024-06-21_modelrun_joint_a_1_b_6_data.RData"
 run <- load(paste0(runpath, runfile))
 # model output
 outfile <- "A_best_alpha/2024-05-10_modelrun_alpha_a_10_b_1_output.csv"
+#outfile <- "2024-06-21_modelrun_joint_a_1_b_6_output.csv"
 out <- read.csv(paste0(outpath, outfile))
 
 # data from model inputs for computing mu
 data <- model_info$metadata$data
 # covariates
-env <- data$a
+enva <- data$a
+envb <- data$b
 
 # predicted intercept and slope means
 preds <- apply(out, 2, mean)
@@ -72,3 +75,82 @@ Emu0 <- as.matrix(env) %*% as.matrix(preds[4:(length(preds)-1)])
 # mun[s] <- R * x[s]
 # x[s] ~ dnorm(x_ic[s], tau_ic[s])
 
+
+
+### plotting:
+
+# libraries
+library(ggplot2)
+library(hrbrthemes)
+library(pROC)
+
+# plot for pred vs obs dist mag
+plot_data = cbind.data.frame(june_2016_cs_m, Emu0)
+plot_data = cbind.data.frame(mags_2016_cs_correct, Emu0)
+#plot_lm <- lm(plot_data$Emu0 ~ plot_data$june_2016_cs_m)
+plot_lm <- lm(plot_data$mags_2016_cs ~ plot_data$Emu0)
+
+mags_plot <- ggplot(plot_data, aes(x = plot_data[,2], y = plot_data[,1])) +
+  geom_point(color = "grey37") +
+  geom_abline(color = "red", lwd = 1)
+  #geom_smooth(method=lm , color="red", se=FALSE)
+print(mags_plot)
+sqrt(mean(plot_data$Emu0-plot_data$mags_2016_cs, na.rm=T)^2)
+
+# plot for ROC/AUC
+plot_data_roc = cbind.data.frame(roc_fit_m, Ed)
+plot_roc <- roc(plot_data_roc$roc_fit_m ~ plot_data_roc$Ed)
+plot_auc <- plot_roc$auc
+
+auc_plot <- ggplot(plot_data_roc, aes(y = plot_data_roc$roc_fit_m,
+                                      x = plot_data_roc$Ed)) +
+  geom_point(color = "grey37") + 
+  #geom_smooth(se = F)
+  abline()
+print(auc_plot)
+
+# RMSE
+sqrt(mean(plot_data_roc$roc_fit_m-plot_data_roc$Ed)^2)
+
+
+### Tables
+# libraries
+library(kableExtra)
+library(knitr)
+library(webshot)
+
+#prep table
+summ = summary(best_alpha_jpout)
+means = formatC(summ$statistics[,"Mean"], 
+                digits = 3,
+                format = 'f')
+alpha_table = as.data.frame(matrix(NA, length(means)+1, 1))
+rownames(alpha_table) <- c(names(means)[1],"Int",
+                           names(dpls[[1]][2:4]),
+                           names(means)[6:7],
+                           "DIC")
+colnames(alpha_table) <- "Value"
+alpha_table[,1] <- c(means, formatC(best_alpha_dic[[2]][1],
+                                    digits = 3,
+                                    format = 'f'))
+kbl(alpha_table) %>%
+  kable_styling()
+#save_kable(alpha_table, "Chapter_1/2024_06_alpha_table.png")
+
+
+
+# beta table
+summ = summary(best_beta_jpout)
+means = formatC(summ$statistics[,"Mean"], 
+                digits = 3,
+                format = 'f')
+beta_table = as.data.frame(matrix(NA, length(means)+1, 1))
+rownames(beta_table) <- c(names(means)[1:2], "Int",
+                          names(dmls[[1]][2:5]),
+                          names(means[8]), "DIC")
+colnames(beta_table) <- "Value"
+beta_table[,1] <- c(means, formatC(best_beta_dic[[2]][1],
+                                    digits = 3,
+                                    format = 'f'))
+kbl(beta_table) %>%
+  kable_styling()
