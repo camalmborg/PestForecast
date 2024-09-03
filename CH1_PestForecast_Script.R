e2145a03872346df8e9a467a1432d0f265784425
camalmborg <malmborg@bu.edu>
2024-09-03 15:18:04 -0400

    IMPORTANT changes to script to make xic and tic for proper disturbance year (2016)

--- CH1_PestForecast_Script.R
+++ CH1_PestForecast_Script.R
@@ -17,13 +17,12 @@ setwd("/projectnb/dietzelab/malmborg/")
 # conditionscores <- read.csv("2022_08_31_DATAGRAB/2022_12_7_sample_score_mean_5k.csv")
 # score_sds <- read.csv("2022_08_31_DATAGRAB/2022_12_7_sample_score_stddev_5k.csv")
 # dmr_data <- read.csv("CHAPTER_1/DATA/2023_12_DMR_DATA_TCG.csv")
-# load("Chapter_1/2024_02_JAGS_models/dmls.RData")
-# load("Chapter_1/2024_02_JAGS_models/dpls.RData")
-load("Ch1_PestForecast/data/2024_04_dmls.RData")
-load("Ch1_PestForecast/data/2024_04_dpls.RData")
+load("Ch1_PestForecast/data/2024_05_dmls.RData")
+load("Ch1_PestForecast/data/2024_05_dpls.RData")
+#load("Ch1_PestForecast/data/2024_05_dp3s.RData")
 # MANUAL: remove SMAP missing values 4/29/2024
 # choose model with SMAP
-find_miss <- last(dmls)
+find_miss <- dmls[[2]]
 missing <- as.numeric(rownames(find_miss[!complete.cases(find_miss),]))
 rm(find_miss)
 # load rest of data:
@@ -38,7 +37,7 @@ dmr_data <- read.csv("Ch1_PestForecast/data/DMR_data.csv")
 # spongy_disturb_b <- read_file("2024_04_06_Ch1_JAGS_MODEL_BETA_VERSION.txt")
 spongy_disturb_a <- read_file("Ch1_PestForecast/JAGS_models/2024_04_06_Ch1_JAGS_MODEL_ALPHA_VERSION.txt")
 spongy_disturb_b <- read_file("Ch1_PestForecast/JAGS_models/2024_04_06_Ch1_JAGS_MODEL_BETA_VERSION.txt")
-#spongy_disturb_joint <- read_file("")
+spongy_disturb_joint <- read_file("Ch1_PestForecast/JAGS_models/2024_05_06_Ch1_JAGS_MODEL_JOINT_VERSION.txt")
 
 ### Function for model runs:
 ##' @param scores forest condition score data >> .csv
@@ -81,33 +80,33 @@ spongy_jags <- function(scores, distyr, dmr, stan_devs,
   distyear = distyr
   # condition score disturbance year onset data
   cs_model <- cs[grep(paste0("^", as.character(distyear)), names(cs))]
-  
-  # disturbance data for sample
-  cs <- cs[dmr$X,]
-  # get disturbance years, previous timestep, and condition score value at dist year for each:
-  dists <- vector()
-  cs_dists <- vector()
-  ptmestp <- vector()
-  ptime <- vector()
-  for (i in 1:nrow(cs)){
-    # get disturbance year colname
-    if (dmr$dpy1[i] > 0) {
-      dists[i] <- colnames(cs[grep(paste0("^", distyear, sep = ""), names(cs))])
-      ptmestp[i] <- colnames(prevtime[grep(paste0("^", distyear, sep = ""), names(prevtime))])
-    } else {
-      dists[i] <- colnames(cs[grep(paste0("^",distyear+1, sep = ""), names(cs))])
-      ptmestp[i] <- colnames(prevtime[grep(paste0("^", distyear+1, sep = ""), names(prevtime))])
-    }
-    # get condition score associated with disturbance year
-    cs_dists[i] <- cs[i, dists[i]]
-    # get previous time step score for correct disturbance year
-    ptime[i] <- prevtime[i, ptmestp[i]]
-  }
-  
-  ## for inputting into model, x initial condition
-  xic <- ptime
+  # initial forest condition (May of disturbance year)
+  xic <- prevtime[grep(paste0("^", as.character(distyear)), names(prevtime))]
+  xic <- xic[dmr$X,]
   # make ptime NAs = 0
   xic[which(is.na(xic))] <- 0
+  
+  # # disturbance data for sample  TAKEN OUT 9/3/2024
+  # cs <- cs[dmr$X,]
+  # # get disturbance years, previous timestep, and condition score value at dist year for each:
+  # dists <- vector()
+  # cs_dists <- vector()
+  # ptmestp <- vector()
+  # ptime <- vector()
+  # for (i in 1:nrow(cs)){
+  #   # get disturbance year colname
+  #   dists[i] <- colnames(cs[grep(paste0("^", distyear, sep = ""), names(cs))])
+  #   ptmestp[i] <- colnames(prevtime[grep(paste0("^", distyear, sep = ""), names(prevtime))])
+  #   # get condition score associated with disturbance year
+  #   cs_dists[i] <- cs[i, dists[i]]
+  #   # get previous time step score for correct disturbance year
+  #   ptime[i] <- prevtime[i, ptmestp[i]]
+  # }
+  
+  # ## for inputting into model, x initial condition
+  # xic <- ptime
+  # # make ptime NAs = 0
+  # xic[which(is.na(xic))] <- 0
   
   
   ### standard deviations for precisions
@@ -137,23 +136,19 @@ spongy_jags <- function(scores, distyr, dmr, stan_devs,
   cs_sds <- vector()
   for (i in 1:nrow(sds)){
     # get condition score sds associated with disturbance year
-    cs_sds[i] <- sds[i, dists[i]]
+    cs_sds[i] <- sds[i, colnames(cs[grep(paste0("^", distyear, sep=""), names(cs))])]
   }
   # fill in NA values before precisions calculation
   # find missing June SD sites
   miss_SD <- which(is.na(cs_sds))
   # find missing dist years for SD NA's
-  miss_dist <- dists[which(is.na(cs_sds))]
+  miss_dist <- cs_sds[which(is.na(cs_sds))]
   # take 75%ile for SDs across sites
-  sd_75s <- c(quantile(cs_sds[which(dists == unique(dists)[1])], 0.75, na.rm = T),
-              quantile(cs_sds[which(dists == unique(dists)[2])], 0.75, na.rm = T))
+  sd_75s <- quantile(cs_sds, 0.75, na.rm = T)
+  # quantile(cs_sds[which(dists == unique(dists)[2])], 0.75, na.rm = T))
   # fill in NAs based on disturbance year
   for (i in 1:length(miss_SD)){
-    if(miss_dist[i] == unique(dists)[1]){
-      cs_sds[miss_SD[i]] <- sd_75s[1]
-    } else {
-      cs_sds[miss_SD[i]] <- sd_75s[2]
-    }
+    cs_sds[miss_SD[i]] <- sd_75s
   }
   
   # do the same for prev year
@@ -161,7 +156,7 @@ spongy_jags <- function(scores, distyr, dmr, stan_devs,
   prev_sds <- vector()
   for (i in 1:nrow(prevsds)){
     # get condition score associated with disturbance year
-    prev_sds[i] <- prevsds[i, ptmestp[i]]
+    prev_sds[i] <- prevsds[i, colnames(prevtime[grep(paste0("^", distyear, sep = ""), names(prevtime))])]
   }
   # fill in NA values before precisions calculation
   # find missing previous time step SD sites
@@ -169,15 +164,11 @@ spongy_jags <- function(scores, distyr, dmr, stan_devs,
   # find missing dist years for SD NA's
   miss_prev <- ptmestp[which(is.na(prev_sds))]
   # take 75%ile for SDs across sites
-  prevsd_75s <- c(quantile(prev_sds[which(ptmestp == unique(ptmestp)[1])], 0.75, na.rm = T),
-                  quantile(prev_sds[which(ptmestp == unique(ptmestp)[2])], 0.75, na.rm = T))
+  prevsd_75s <- quantile(prev_sds, 0.75, na.rm = T)
+  #quantile(prev_sds[which(ptmestp == unique(ptmestp)[2])], 0.75, na.rm = T))
   # fill in NAs based on previous year
   for (i in 1:length(miss_pSD)){
-    if(miss_prev[i] == unique(ptmestp)[1]){
-      prev_sds[miss_pSD[i]] <- prevsd_75s[1]
-    } else {
-      prev_sds[miss_pSD[i]] <- prevsd_75s[2]
-    }
+    prev_sds[miss_pSD[i]] <- prevsd_75s
   }
   
   # convert SDs to precisions
@@ -225,8 +216,8 @@ spongy_jags <- function(scores, distyr, dmr, stan_devs,
   # dmls matrix list 
   covls <- dmls
   # test data object
-  dat <- list(y = cs_dists,
-              dist = as.numeric(cs_dists < -1),
+  dat <- list(y = cs_model,
+              dist = as.numeric(cs_model < -1),
               covls = covls)
   
   # make empty list to fill in magnitude covariates
@@ -251,8 +242,8 @@ spongy_jags <- function(scores, distyr, dmr, stan_devs,
   # test object of covariates
   covls <- dpls
   # make test data object
-  dat <- list(y = cs_dists,
-              dist = as.numeric(cs_dists < -1),
+  dat <- list(y = cs_model,
+              dist = as.numeric(cs_model < -1),
               covls = covls)
   
   alpha.init <- list()
@@ -365,7 +356,8 @@ spongy_jags <- function(scores, distyr, dmr, stan_devs,
   j.pests <- jags.model(file = textConnection(model),
                         data = data,
                         inits = init,
-                        n.chains = 3)
+                        n.chains = 4,
+                        n.adapt = 5000)
   
   # get outputs
   jpout<-coda.samples(j.pests,
@@ -408,13 +400,13 @@ model_save <- function(jagsmodel, vartype){
                              date, 
                              "_modelrun_", filepath_var[vartype],
                              "a_", as.character(jagsmodel$metadata$modelrun_a),
-                             "b_", as.character(jagsmodel$metadata$modelrun_b),
+                             "_b_", as.character(jagsmodel$metadata$modelrun_b),
                              "_output",".csv")
   filename_runs <- paste0(filepath_runs,
                           date,
                           "_modelrun_", filepath_var[vartype],
                           "a_", as.character(jagsmodel$metadata$modelrun_a),
-                          "b_", as.character(jagsmodel$metadata$modelrun_b),
+                          "_b_", as.character(jagsmodel$metadata$modelrun_b),
                           "_data",".RData")
   
   # save outputs to folder
@@ -463,29 +455,68 @@ model_save <- function(jagsmodel, vartype){
 # 2024-04-22 
 # running alpha models
 
-# variables want in outputs from jags model
-vars <- c("beta0", "alpha", "R", "pa0")
-iters = 200000
-thin = 20
-diters = 10000
+# # variables want in outputs from jags model
+# #vars <- c("beta", "alpha0", "R", "pa0")
+# #vars <- c("beta", "alpha0", "R", "pa0")
+# iters = 250000
+# thin = 20
+# diters = 10000
 
-# loop for re-running beta models: 4/29/2024
-for (i in 1:2){
-  model <- spongy_jags(scores = conditionscores, 
-                       distyr = 2016, 
-                       dmr = dmr_data, 
-                       stan_devs = scores_sds, 
-                       dmls = dmls, 
-                       dpls = dpls,
-                       modelrun_a = 1, 
-                       modelrun_b = i, 
-                       model = spongy_disturb_b, 
-                       covs = 2, 
-                       vars = vars, 
-                       iters = iters, 
-                       thin = thin, 
-                       diters = diters, 
-                       missing = missing)
-  model_save(model, 2)
-  rm(model)
-}
+# # for running "best" beta models (from exploratory analyses) 5/1/2024
+# # 3, 4, 10, 12, 13, 15, 17
+#biobest <- c(10) #distmag
+#biobest <- c(15, 17, 18)  #distprob
+#dmruns <- c(11,13,14,15,17)  #best jags models - distmag
+#dpruns <- c(10,11,14,17,18)    #best jags models - distprob
+# for running a quick beta model 
+#dmls_t <- dmls[[13]]
+#dmls_tt <- dmls_t[,c(1,2,4,6,7)] #is the 1,3,5,6
+# 
+# # loop for re-running beta models: 4/30/2024
+# for (i in 1:3){
+#   model <- spongy_jags(scores = conditionscores,
+#                        distyr = 2016,
+#                        dmr = dmr_data,
+#                        stan_devs = scores_sds,
+#                        dmls = dmls,
+#                        dpls = dpls,
+#                        modelrun_a = i,  # i for running alpha models (1 for beta)
+#                        modelrun_b = 1,  # i for running beta models (1 for alpha)
+#                        model = spongy_disturb_a,   # spongy_disturb > _a for alpha, _b for beta
+#                        covs = 1,        # 1 = alpha, 2 = beta
+#                        vars = vars,
+#                        iters = iters,
+#                        thin = thin,
+#                        diters = diters,
+#                        missing = missing)
+#   model_save(model, 1)    # 1 for alpha, 2 for beta, 3 for joint
+#   rm(model)
+# }
+
+
+# ### testing joint model:
+
+# variables want in outputs from jags model
+vars <- c("beta", "alpha", "R", "pa0")
+iters = 300000
+thin = 30
+diters = 25000
+
+
+model <- spongy_jags(scores = conditionscores,
+                     distyr = 2016,
+                     dmr = dmr_data,
+                     stan_devs = scores_sds,
+                     dmls = dmls,
+                     dpls = dpls,
+                     modelrun_a = 2,  # i for running alpha models (1 for beta)
+                     modelrun_b = 6,  # i for running beta models (1 for alpha)
+                     model = spongy_disturb_joint,   # spongy_disturb > _a for alpha, _b for beta
+                     covs = 3,        # 1 = alpha, 2 = beta
+                     vars = vars,
+                     iters = iters,
+                     thin = thin,
+                     diters = diters,
+                     missing = missing)
+model_save(model, 3)    # 1 for alpha, 2 for beta
+#rm(model)
