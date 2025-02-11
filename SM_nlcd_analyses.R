d9dab6cc19ab83cbcb07be09ac68ee2ba84c1b09
Charlotte Malmborg <charlottemalmborg@Charlottes-MacBook-Pro.local>
2025-02-11 15:33:53 -0500

    adding tree percent cover and land cover type to gam analyses for dist mag and dist prob

--- SM_nlcd_analyses.R
+++ SM_nlcd_analyses.R
@@ -23,7 +23,7 @@ tcg <- read.csv(tfile)
 cfile <- "2022_08_31_DATAGRAB/2022_12_7_sample_score_mean_5k.csv" #1995-2020, original grab
 scores <- read.csv(cfile)
 # NLCD data
-nlcdfile <- "data_archive/2020_07_10_500_sample_nlcd.csv"
+nlcdfile <- "CHAPTER_1/DATA/2020_07_10_sample_nlcd.csv"
 nlcd <- read.csv(nlcdfile) ## checked if sites match, they do, we're good!
 # select sites without missing dmr data
 nlcd <- nlcd[dmr$X,]
@@ -37,18 +37,19 @@ nlcd_cover <- unique(nlcd$landcover)
 # make data object
 # nlcd variable
 lc <- nlcd$landcover
+ptc <- nlcd$percent_tree_cover
 # change 41 (deciduous forest class) to 1 for setting reference class in gam
 lc <- replace(lc, lc == 41, 1)
 # column number for disturbance years
 cn <- as.matrix(as.numeric(dmr$colnum))
 # which response variable? "mags" for dist mag, "dpy1" or "dpy2" for dist prob
-yvar <- "mags"
-#yvar <- "dpy1"
+#yvar <- "mags"
+yvar <- "dpy1"
 #yvar <- "dpy2"
 y <- as.matrix(as.numeric(dmr[,yvar]))
 # make data frame to convert to data
-x <- as.data.frame(cbind(y, cn, lc))
-colnames(x) <- c("y", "cn", "lc")
+x <- as.data.frame(cbind(y, cn, lc, ptc))
+colnames(x) <- c("y", "cn", "lc", "ptc")
 # data object for gams
 #select disturbance year column number -- 22 for 2016, 23 for 2017
 coln = 22
@@ -59,21 +60,31 @@ dat <- dat[order(dat$lc),]
 
 # run model
 # disturbance magnitude
-nlcd_gam <- gam(y ~ as.factor(lc), data = dat, method = "REML")
-# disturbance probability
-#nlcd_gam <- gam(y ~ as.factor(lc), data = dat, family = "binomial", method = "REML")
-
+nlcd_gam <- gam(y ~ as.factor(lc), data = dat)
 # check model summary
 summ <- summary(nlcd_gam)
 r2 <- summ$r.sq
-#aic <- nlcd_gam$aic
+aic <- nlcd_gam$aic
+#ran 2/11/2025: r2 = 0.186744, aic = -4278.522635 for y = mags
+#ran 2/11/2025: r2 = 0.090457, aic = 2002.571447 for y = dpy1
+
+# do with percent tree cover
+nlcd_ptc_gam <- gam(y ~ as.factor(ptc), data = dat)
+summ <- summary(nlcd_ptc_gam)
+r2 <- summ$r.sq
+aic <- nlcd_ptc_gam$aic
+#ran 2/11/2025 r2 = 0.0984026, aic = -4084.3285187 for y = mags
+#ran 2/11/2025 r2 = 0.0457663, aic = 2098.2506737 for y = dpy1
+
+# disturbance probability
+nlcd_gam <- gam(y ~ as.factor(lc), data = dat, family = "binomial", method = "REML")
 
 # if doing disturbance probability, ROCs:
 # run ROC
-nlcd_roc <- roc(dat$y, nlcd_gam$fitted.values)
+nlcd_roc <- multiclass.roc(dmr$dpy1, lc)
 roc <- nlcd_roc$auc
-#aic <- nlcd_gam$aic
-
+aic <- nlcd_gam$aic
+#ran 2/11/25 roc = 0.454, aic = 1938.28555
 
 # notes:
 # convert categories to factor
